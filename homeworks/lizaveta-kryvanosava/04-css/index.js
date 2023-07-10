a70ef5d82f287ad93f9a549733c682c1438f84eb
Rivenrem <rivensenik@gmail.com>
2023-07-11 01:06:06 +0300

    feat: create a portfolio page

---
+++ homeworks/lizaveta-kryvanosava/04-css/index.js
@@ -0,0 +1,23 @@
+let currentTabInPortfolio = document.querySelector(".portfolio__content");
+
+document.querySelector(".theme-toggle").addEventListener("click", changeTheme);
+
+document
+  .querySelector("#tabToggle")
+  .addEventListener("change", changeTabInPortfolio);
+
+function changeTheme() {
+  document.documentElement.setAttribute(
+    "color-scheme",
+    document.documentElement.getAttribute("color-scheme") === "light"
+      ? "dark"
+      : "light"
+  );
+}
+
+function changeTabInPortfolio(event) {
+  currentTabInPortfolio.classList.add("hidden");
+  currentTabInPortfolio = document.querySelector(`.${event.target.value}`);
+  currentTabInPortfolio.classList.remove("hidden");
+  console.log(currentTabInPortfolio);
+}
