7ed8f92ba81ec95d741c5d1adb32a945c08703f5
urbanmatias <murban.laba@solvd.com>
2023-07-11 11:23:40 -0300

    finished homework #4 CSS

---
+++ homeworks/matias-urban/[Homework #4] - CSS by matias urban/js/dark-mode.js
@@ -0,0 +1,62 @@
+const btn =
+  document.querySelector("#btn");
+const elemColor1 =
+  document.querySelectorAll(".color-1");
+const links = document.querySelectorAll(
+  ".link-icons"
+);
+const buttonDownload =
+  document.querySelector(
+    ".container-buttons__download"
+  );
+const icons =
+  document.querySelectorAll(".icon");
+const title =
+  document.querySelector("#title");
+const buttonArea =
+  document.querySelector(
+    ".button-container"
+  );
+const buttonPortfolio =
+  document.querySelector(
+    ".button-container__button1"
+  );
+const buttonSkills =
+  document.querySelector(
+    ".button-container__button2"
+  );
+btn.addEventListener("click", () => {
+  console.log("click");
+  document.body.classList.toggle(
+    "background-color-0c151d"
+  );
+  title.classList.toggle(
+    "color-F1F2F4"
+  );
+  buttonArea.classList.toggle(
+    "background-color-171F26"
+  );
+  buttonPortfolio.classList.toggle(
+    "background-color-0c151d"
+  );
+  btn.classList.toggle(
+    "background-color-171F26"
+  );
+  buttonSkills.classList.toggle(
+    "background-color-171F26"
+  );
+  buttonDownload.classList.toggle(
+    "background-color-ffe071"
+  );
+  for (const elem of elemColor1) {
+    elem.classList.toggle(
+      "color-a3abb2"
+    );
+  }
+  for (const icon of links) {
+    icon.classList.toggle("no-show");
+  }
+  for (const icon of icons) {
+    icon.classList.toggle("no-show");
+  }
+});
