1457c3917518305923359d1a87d8c1958c2601b7
Rivenrem <rivensenik@gmail.com>
2023-07-11 21:12:17 +0300

    feat: add keyframes animation on modal window

--- homeworks/lizaveta-kryvanosava/05-css-advanced/src/scripts/modal.js
+++ homeworks/lizaveta-kryvanosava/05-css-advanced/src/scripts/modal.js
@@ -1,12 +1,20 @@
 const modal = document.querySelector('#modal');
+const form = document.querySelector('.modal__form');
 
 const openButton = document.querySelector('#open-modal');
 const closeButton = document.querySelector('#close-modal');
 
 openButton.onclick = function () {
   modal.style.display = 'flex';
+  form.style.animation = 'scale-in 0.5s ease-in-out';
 };
 
 closeButton.onclick = function () {
-  modal.style.display = 'none';
+  form.style.animation = 'scale-out .5s ease-in-out';
 };
+
+form.addEventListener('animationend', ({animationName}) => {
+  if (animationName === 'scale-out') {
+    modal.style.display = 'none';
+  }
+});
