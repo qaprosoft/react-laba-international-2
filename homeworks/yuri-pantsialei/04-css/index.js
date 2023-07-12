834352fc2be9fec0fc381f885cb70c93ae068977
Yura <jormungand1616@gmail.com>
2023-07-12 18:49:11 +0300

    use prettier

--- homeworks/yuri-pantsialei/04-css/index.js
+++ homeworks/yuri-pantsialei/04-css/index.js
@@ -1,19 +1,24 @@
 // Get the button
-let mybutton = document.getElementById("goToTopBtn");
+let mybutton = document.getElementById('goToTopBtn');
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 && window.innerWidth < 601) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 20 ||
+    (document.documentElement.scrollTop > 20 && window.innerWidth < 601)
+  ) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTo({ top: 0, behavior: 'smooth' });
-    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
+  document.body.scrollTo({top: 0, behavior: 'smooth'});
+  document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
 }
