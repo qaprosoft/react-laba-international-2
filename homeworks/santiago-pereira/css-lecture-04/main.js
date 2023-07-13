d6d773b52efb19fedd98c7538335876f07ba6222
Santiago-Pereira <santiagopereira_8@hotmail.com>
2023-07-13 17:44:09 -0300

    sp-css-lecture-04

---
+++ homeworks/santiago-pereira/css-lecture-04/main.js
@@ -0,0 +1,22 @@
+/* selectors */
+const darkModeBtn = document.querySelector(".theme-btn");
+const mediaIconsBright = document.querySelector(".profile-social-media-bright")
+const mediaIconsDark = document.querySelector(".profile-social-media")
+console.log(mediaIconsBright,"bright");
+
+/* event listener to change visual mode */
+darkModeBtn.addEventListener("click", ()  =>{
+    console.log("i was clicked");
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        darkModeBtn.src = "./assets/images/images-phone-size/other-icons/dark-icon.svg";
+        mediaIconsBright.style.display = "none";
+        mediaIconsBright.style.display = "flex";
+    }else{
+        darkModeBtn.src = "./assets/images/images-phone-size/other-icons/bright-mode.svg";
+        mediaIconsDark.style.display = "none";
+        mediaIconsBright.style.display = "flex";
+    }
+    
+    /* document.body.classList.contains("dark-theme") ? darkModeBtn.src = "./assets/images/images-phone-size/other-icons/dark-icon.svg" : darkModeBtn.src = "./assets/images/images-phone-size/other-icons/bright-mode.svg"; */
+})
