4bca4d82a8bf2c3803faa16f3f7953cd1d2b3670
Antonina Vechorko <antonina.vechorko@gmail.com>
2023-07-16 21:36:31 +0300

    feat: add JS module, local storage for toggle

---
+++ homeworks/antonina-vechorko/05-css-advanced/script/scroll-up.js
@@ -0,0 +1,28 @@
+const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
+
+document.addEventListener('DOMContentLoaded', () => {
+    scrollToTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    const handleScroll = () => {
+        const scrollPosition = window.pageYOffset;
+        const viewportWidth = window.innerWidth;
+
+        if (viewportWidth <= 767 && scrollPosition > 500) {
+            // Show the button
+            scrollToTopBtn.style.display = 'block';
+        } else {
+            // Hide the button
+            scrollToTopBtn.style.display = 'none';
+        }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleScroll);
+
+    handleScroll();
+});
