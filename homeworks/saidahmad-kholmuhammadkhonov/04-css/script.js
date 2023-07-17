3e24c0a1553cb6001165927ceb059ebc742654a6
unknown <saidahmadkhon001@gmail.com>
2023-07-17 22:28:04 +0200

    rewritten css according to BEM

--- homeworks/saidahmad-kholmuhammadkhonov/04-css/script.js
+++ homeworks/saidahmad-kholmuhammadkhonov/04-css/script.js
@@ -1,4 +1,4 @@
-const themeToggle = document.getElementById('set-theme');
+const themeToggle = document.getElementById('theme-switcher');
 const body = document.querySelector('body');
 
 themeToggle.addEventListener('click', () => {
