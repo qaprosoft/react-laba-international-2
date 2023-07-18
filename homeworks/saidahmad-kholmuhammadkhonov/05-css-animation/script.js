413bd356b41562a7f905f687f15e67b35b72bf31
unknown <saidahmadkhon001@gmail.com>
2023-07-18 22:59:37 +0200

    added css animations

---
+++ homeworks/saidahmad-kholmuhammadkhonov/05-css-animation/script.js
@@ -0,0 +1,20 @@
+const themeToggle = document.getElementById('theme-switcher');
+const body = document.querySelector('body');
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
