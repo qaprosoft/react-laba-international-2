5acd19d924a4a2547fe7a230392301f7bc637f99
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-07-23 16:33:51 +0300

    added: set up responsive layout with media.css file for mobiles and tablets

--- homeworks/oleg-gorodenskyi/05-css-advanced/js/script.js
+++ homeworks/oleg-gorodenskyi/05-css-advanced/js/script.js
@@ -1,29 +1,40 @@
 window.addEventListener("DOMContentLoaded", function() {
-  const tabs = document.querySelectorAll('.tabs__item');
-  const cards = document.querySelectorAll('.portfolio__card');
 
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", function() {
-      for (let j = 0; j < tabs.length; j++) {
-        tabs[j].classList.remove("active");
+    const tabs = document.querySelectorAll('.tabs__item');
+    const cards = document.querySelectorAll('.portfolio__card');
+    const pageup = document.querySelector('.pageup');
+
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener("click", function() {
+        for (let j = 0; j < tabs.length; j++) {
+          tabs[j].classList.remove("active");
+        }
+        this.classList.add("active");
+      });
+    }
+
+    function handleScroll() {
+      cards.forEach(function(card) {
+        const cardTop = card.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+    
+        if (cardTop < windowHeight) {
+          card.classList.add('show');
+        }
+      });
+    }
+
+    function scrollToTop() {
+      if (window.scrollY > 300 && window.innerWidth <= 599) {
+        pageup.classList.add('show');
+      } else {
+        pageup.classList.remove('show');
       }
-      this.classList.add("active");
-    });
-  }
+    }
 
-  function handleScroll() {
-    cards.forEach(function(card) {
-      const cardTop = card.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
+    this.window.addEventListener('scroll', handleScroll)
+    this.window.addEventListener('scroll', scrollToTop)
 
-      console.log(cardTop, windowHeight)
-      if (cardTop < windowHeight) {
-        card.classList.add('show');
-      }
-    });
-  }
+  });
+  
 
-  this.window.addEventListener('scroll', handleScroll)
-
-
-});
