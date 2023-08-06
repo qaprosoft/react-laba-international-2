8923e9f56f15bd3fff74a7967ce3158227b81360
Antonina Vechorko <antonina.vechorko@gmail.com>
2023-08-07 01:38:01 +0300

    feat: finish hw-7

--- homeworks/antonina-vechorko/07-js-basics-2/index.js
+++ homeworks/antonina-vechorko/07-js-basics-2/index.js
@@ -148,5 +148,78 @@ function moveZeros(arr) {
 }
 
 // task 3 https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3
+function findUniq(arr) {
+  const formatted = arr.map(str => {
+    const arr = str.trim().toLowerCase().split('');
+    return [...new Set(arr)].sort().join('');
+  });
 
-// https://www.codewars.com/kata/5296bc77afba8baa690002d7
+  for (let i = 0; i < formatted.length; i++) {
+    if (
+      formatted.indexOf(formatted[i]) === formatted.lastIndexOf(formatted[i])
+    ) {
+      return arr[i];
+    }
+  }
+}
+
+// task 4 https://www.codewars.com/kata/5296bc77afba8baa690002d7
+function sudoku(puzzle) {
+  let arr = createArr(puzzle);
+  const sum = 45;
+
+  while (arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      let array = [];
+      array = checkSquare(puzzle, arr[i][0], arr[i][1], array);
+      array = checkLine(puzzle, arr[i][0], arr[i][1], array);
+      array = array.filter((item, index) => array.indexOf(item) === index);
+
+      if (array.length === 8) {
+        puzzle[arr[i][0]][arr[i][1]] =
+          sum - array.reduce((item, s) => item + s);
+      }
+    }
+    arr = createArr(puzzle);
+  }
+
+  return puzzle;
+}
+
+function createArr(puzzle) {
+  const arr = [];
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle.length; j++) {
+      if (puzzle[i][j] === 0) {
+        arr.push([i, j]);
+      }
+    }
+  }
+  return arr;
+}
+
+function checkSquare(puzzle, i, j, array) {
+  let n = i - (i % 3),
+    m = j - (j % 3);
+
+  for (let x = n; x < n + 3; x++) {
+    for (let y = m; y < m + 3; y++) {
+      if (puzzle[x][y]) {
+        array.push(puzzle[x][y]);
+      }
+    }
+  }
+  return array;
+}
+
+function checkLine(puzzle, i, j, array) {
+  for (let x = 0; x < 9; x++) {
+    if (puzzle[i][x]) {
+      array.push(puzzle[i][x]);
+    }
+    if (puzzle[x][j]) {
+      array.push(puzzle[x][j]);
+    }
+  }
+  return array;
+}
