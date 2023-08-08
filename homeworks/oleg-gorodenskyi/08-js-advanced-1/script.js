cc499c57a9eb56b06e05735a2b0e44f203f4d380
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-08-08 18:55:31 +0300

    added task 6

--- homeworks/oleg-gorodenskyi/08-js-advanced-1/script.js
+++ homeworks/oleg-gorodenskyi/08-js-advanced-1/script.js
@@ -68,6 +68,23 @@ function objConcat(o) {
     return result
 }
 
+// task 6 https://www.codewars.com/kata/547f1a8d4a437abdf800055c
+function NamedOne(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    
+    Object.defineProperty(this, 'fullName', {
+        get: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        set: function (value) {
+        if (value.match(/\w+ \w+/))
+          return  [this.firstName, this.lastName] = value.split(' ')
+        }
+    })
+}
+
 // Optional (advanced)
 
 // task 9 https://www.codewars.com/kata/partial-keys
