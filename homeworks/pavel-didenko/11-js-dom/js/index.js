718426d4b2ac571302de697208ca41f2dae36d20
Pavel Didenko <patriot2006_06@mail.ru>
2023-08-12 07:21:32 +0300

    feat: implement selection of several elements by holding Shift key

--- homeworks/pavel-didenko/11-js-dom/js/index.js
+++ homeworks/pavel-didenko/11-js-dom/js/index.js
@@ -2,7 +2,7 @@ const grid = document.querySelector('.cells__wrapper');
 const rows = 30;
 const colls = 20;
 
-function createCell(coll, row){
+function createCell(coll, row) {
   const div = document.createElement('div');
   div.className = 'cell';
   const p = document.createElement('p');
@@ -12,25 +12,38 @@ function createCell(coll, row){
   return div;
 }
 
-function fillGrid(colls, rows, grid){
+function fillGrid(colls, rows, grid) {
   const cellNumber = colls * rows;
   for (let i = 0; i < cellNumber; i++) {
     let rowsCount = Math.floor(i / colls) + 1;
-    let collsCount = i % colls + 1;
+    let collsCount = (i % colls) + 1;
     grid.appendChild(createCell(collsCount, rowsCount));
   }
 }
 
 fillGrid(colls, rows, grid);
 
+grid.addEventListener('click', function (event) {
+  const selectedCell =
+    event.target.tagName === 'DIV' ? event.target : event.target.parentNode;
+  const column = selectedCell.firstChild.innerHTML.split('/')[0];
+  const row = selectedCell.firstChild.innerHTML.split('/')[1];
+  const selectedCellColor = 'rgb(0, 65, 169)';
+  const selectedRowColumnColor = 'aqua';
 
-grid.addEventListener('click', function(event){
-  for(let child of this.children){
-    child.style.backgroundColor = "#FFFFFF";
-    child.firstChild.style.display = 'none';
+  for (let child of this.children) {
+    if (
+      child.firstChild.innerHTML.split('/')[0] === column ||
+      child.firstChild.innerHTML.split('/')[1] === row
+    ) {
+      child.style.backgroundColor = selectedRowColumnColor;
+      child.firstChild.style.display = 'block';
+    } else if (!event.shiftKey) {
+      child.style.backgroundColor = '#FFFFFF';
+      child.firstChild.style.display = 'none';
+    }
   }
 
-  event.target.style.backgroundColor = 'aqua';
-  event.target.firstChild.style.display = 'block';
-})
-
+  selectedCell.style.backgroundColor = selectedCellColor;
+  selectedCell.firstChild.style.display = 'block';
+});
