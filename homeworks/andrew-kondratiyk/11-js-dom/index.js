5e8a5798703bcd38f103525206900d8945b6eb14
Andrew <gandrejko2002@gmail.com>
2023-08-13 13:36:51 +0300

    implemented selecting with shift

--- homeworks/andrew-kondratiyk/11-js-dom/index.js
+++ homeworks/andrew-kondratiyk/11-js-dom/index.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 
 const ROWS_COUNT = 20;
 const COLS_COUNT = 30;
+let shiftPressed = false;
 
 const createCell = (x, y) => {
   const cell = document.createElement('div');
@@ -12,9 +13,11 @@ const createCell = (x, y) => {
 
   cell.addEventListener('click', function(event) {
     const cells = document.querySelectorAll('.cell');
-    cells.forEach((cell) => {
-      cell.classList.remove('active', 'activeRowOrCol');
-    });
+    if(!shiftPressed) {
+      cells.forEach((cell) => {
+        cell.classList.remove('active', 'activeRowOrCol');
+      });
+    }
     cell.classList.add('active');
     cells.forEach((cell) => {
       if(this.dataset.x === cell.dataset.x || this.dataset.y === cell.dataset.y) {
@@ -30,8 +33,21 @@ const createCell = (x, y) => {
   return cell;
 }
 
-for (let i = 0; i < ROWS_COUNT; i++) {
-  for (let j = 0; j < COLS_COUNT; j++) {
-    container.appendChild(createCell(i, j));
+window.addEventListener('keydown', (event) => {
+  if(event.key === 'Shift') {
+    shiftPressed = true;
   }
-}
+});
+window.addEventListener('keyup', (event) => {
+  if(event.key === 'Shift') {
+    shiftPressed = false;
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  for (let i = 0; i < ROWS_COUNT; i++) {
+    for (let j = 0; j < COLS_COUNT; j++) {
+      container.appendChild(createCell(i, j));
+    }
+  }
+})
