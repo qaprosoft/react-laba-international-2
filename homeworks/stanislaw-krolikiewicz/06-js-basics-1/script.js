31f367f813c76f6e41f76ba8007f3b459cc9ca27
stkrolikiewicz <st.krolikiewicz@gmail.com>
2023-08-13 18:15:31 +0200

    feat: Add solutions to tasks 9-14

--- homeworks/stanislaw-krolikiewicz/06-js-basics-1/script.js
+++ homeworks/stanislaw-krolikiewicz/06-js-basics-1/script.js
@@ -151,3 +151,103 @@ const partitionOn = (pred, arr) => {
 
 // task 8 http://www.codewars.com/kata/word-count
 // Error 404
+
+// task 9 https://www.codewars.com/kata/find-the-odd-int/
+const findOdd = (Array) => {
+  let activeNumber = Array[0], counter = 0
+
+  for (let i = 0; i < Array.length; i++) {
+    if (Array[i] === activeNumber) counter++
+    if (i === Array.length - 1) {
+      if (counter % 2 === 0) return findOdd(Array.filter(item => item !== activeNumber))
+      else return activeNumber
+    }
+  }
+}
+
+// task 10 https://www.codewars.com/kata/find-the-parity-outlier
+const findOutlier = (Array) => {
+  if(Math.abs(Array[0]) % 2 === 0) {
+    if (Array[1] % 2 === 0)
+      for (let i = 2; i < Array.length; i++) {
+        if (Math.abs(Array[i]) % 2 === 1) return Array[i]
+      }
+    else {
+      if (Math.abs(Array[2]) % 2 === 1) return Array[0]
+      else return Array[1]
+    }
+  }
+  else {
+    if (Math.abs(Array[1]) % 2 === 1)
+      for (let i = 2; i < Array.length; i++) {
+        if (Math.abs(Array[i]) % 2 === 0) return Array[i]
+      }
+    else {
+      if (Math.abs(Array[2]) % 2 === 1) return Array[1]
+      else return Array[0]
+    }
+  }
+}
+
+// task 11 https://www.codewars.com/kata/zipwith
+const zipWith = (fn, a0,a1) => {
+  let l = Math.min(a0.length, a1.length)
+  let finalArray = []
+  for (let i = 0; i < l; i++) finalArray.push(fn(a0[i], a1[i]))
+  return finalArray
+}
+
+// task 12 https://www.codewars.com/kata/filter-the-number
+const filterString = string => {
+  let filtered = ''
+  Array.from(string).forEach(char => {
+    if (char >= 0 && char <= 9) filtered += char
+  })
+  return parseInt(filtered)
+}
+
+// task 13 https://www.codewars.com/kata/n-th-fibonacci
+const nthFibo = n => {
+  let sum = 1, lastN = 1, lastLastN = 0;
+  if (n === 1) sum = 0
+  else if (n > 2) {
+    for (let i = 2; i < n; i++) {
+      sum = lastN + lastLastN
+      lastLastN = lastN
+      lastN = sum
+    }
+  }
+  return sum
+}
+
+// const nthFibo = n => {
+//   let [prev, curr] = [0, 1]
+//   for (let i = 1; i < n; i++) [prev, curr] = [curr, prev + curr]
+//   return prev
+// }
+
+// task 14 https://www.codewars.com/kata/cat-and-mouse-2d-version/
+const catMouse = (map, moves) => {
+  let C = {row: undefined, col: undefined}, m = {row: undefined, col: undefined}, minMoves, width = 0
+  while (map[width] === '.' || map[width] === 'C' || map[width] === 'm') {
+    width++
+  }
+  width++
+  for (let i = 0; i < map.length; i++) {
+    if (map[i] === 'C') {
+      C.row = parseInt(i / width) + 1
+      C.col = i % width + 1
+    }
+    else if (map[i] === 'm') {
+      m.row = parseInt(i / width) + 1
+      m.col = i % width + 1
+    }
+  }
+  minMoves = Math.abs(C.row - m.row) + Math.abs(C.col - m.col)
+  console.log(minMoves, C.col, C.row, m.col, m.row)
+  if (m.row && C.col) {
+    if (minMoves <= moves) return 'Caught!'
+    else return 'Escaped!'
+  }
+  else return 'boring without two animals'
+}
