d496829568219bc6513d77712643ea1b26f4cc91
ViVse <ivanisenko730@gmail.com>
2023-08-14 14:23:50 +0300

    feat: added demonstrations for cases of usage

--- homeworks/ivan-ishchenko/09-js-advanced-2/index.js
+++ homeworks/ivan-ishchenko/09-js-advanced-2/index.js
@@ -60,3 +60,72 @@ class Serializable {
     }
   }
 }
+
+// Usage examples
+// Serelization & deserialization
+class UserDTO extends Serializable {
+  constructor({firstName, lastName, phone, birth} = {}) {
+    super();
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.birth = birth;
+  }
+
+  printInfo() {
+    console.log(
+      `${this.firstName[0]}. ${this.lastName} - ${
+        this.phone
+      }, ${this.birth.toISOString()}`,
+    );
+  }
+}
+
+let tolik = new UserDTO({
+  firstName: 'Anatoliy',
+  lastName: 'Nashovich',
+  phone: '2020327',
+  birth: new Date('1999-01-02'),
+});
+
+tolik.printInfo(); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
+
+let serialized = tolik.serialize();
+console.log(serialized);
+tolik = null;
+
+const resurrectedTolik = new UserDTO().wakeFrom(serialized);
+
+console.log(resurrectedTolik instanceof Serializable); // true
+resurrectedTolik.printInfo(); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
+
+// Attempt to wake from a serialized line of other class throws an exception
+class Post extends Serializable {
+  constructor({content, date, author} = {}) {
+    super();
+
+    this.content = content;
+    this.date = date;
+    this.author = author;
+  }
+}
+
+try {
+  new Post().wakeFrom(serialized); // Error: Incompatible type of deserialization
+} catch(e) {
+  console.log(e.message)
+}
+
+// NaN as well as Infinities are not lost after the serialization, -0 is turned to 0
+let testPost = new Post({
+  content: NaN,
+  date: -Infinity,
+  author: -0,
+});
+
+serialized = testPost.serialize();
+console.log(serialized);
+testPost = null;
+
+console.log(new Post().wakeFrom(serialized));
