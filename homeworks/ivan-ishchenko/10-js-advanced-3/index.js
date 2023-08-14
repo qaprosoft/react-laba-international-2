35ca430824af7101b961e93696289087538ecd92
ViVse <ivanisenko730@gmail.com>
2023-08-14 15:02:32 +0300

    feat: completed codewars tasks

---
+++ homeworks/ivan-ishchenko/10-js-advanced-3/index.js
@@ -0,0 +1,33 @@
+// task 1 https://www.codewars.com/kata/55e7650c8d894146be000095/javascript
+function validateMessage(msg) {
+  if (msg === null) throw new ReferenceError('Message is null!');
+  if (typeof msg !== 'string')
+    throw new TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`,
+    );
+  if (msg.length > 255 || msg.length === 0)
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  if (msg.match(/<[^>]*>/)) return false;
+  return true;
+}
+
+// task 2 https://www.codewars.com/kata/5a353a478f27f244a1000076
+const sayJoke = async (apiUrl, jokeId) => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  if (!data.hasOwnProperty('jokes')) {
+    throw new Error(`No jokes at url: ${apiUrl}`);
+  }
+
+  const joke = data.jokes.find(joke => joke.id === jokeId);
+
+  if (!joke) {
+    throw new Error(`No jokes found id: ${jokeId}`);
+  }
+
+  return {
+    saySetup: () => joke.setup,
+    sayPunchLine: () => joke.punchLine,
+  };
+};
