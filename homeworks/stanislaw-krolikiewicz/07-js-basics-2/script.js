f109a3cd51addb9fca324916367814ea757ecc92
stkrolikiewicz <st.krolikiewicz@gmail.com>
2023-08-16 17:35:30 +0200

    feat: Add solutions to tasks 10-11 && 1-4 (optional)

--- homeworks/stanislaw-krolikiewicz/07-js-basics-2/script.js
+++ homeworks/stanislaw-krolikiewicz/07-js-basics-2/script.js
@@ -67,3 +67,128 @@ const findUniq = arr => {
 }
 
 // task 10 https://www.codewars.com/kata/581e014b55f2c52bb00000f8
+const decipherThis = str => {
+  let arr = str.split(' '), b = []
+  arr.forEach(el => {
+    let current = '', i
+    console.log(current)
+    if (el[2] >= 0 && el[2] <= 9) i = 3
+    else  i = 2
+    let letters = el.substring(i, el.length)
+    let substitute = letters.length ? letters.length > 1 ? letters[letters.length - 1] + letters.substring(1, letters.length - 1) + letters[0] : letters[0] : ''
+    current = current + String.fromCharCode(el.substring(0, i)) + substitute
+    b.push(current)
+  });
+  return b.join(" ")
+};
+
+// task 11 https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+const sortArray = array => {
+  let odds = [], sorted = [], i = 0
+  array.forEach(el => Math.abs(el) % 2 === 1 && odds.push( el))
+  odds.sort((a,b) => a-b)
+  array.forEach(el => {
+    if (Math.abs(el) % 2 === 1) {
+      sorted.push(odds[i])
+      i++
+    } else sorted.push(el)
+  })
+  return sorted
+}
+
+// Optional
+// task 1 https://www.codewars.com/kata/515bb423de843ea99400000a
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+	// The constructor takes in an array of items and a integer indicating how many
+	// items fit within a single page
+    this.collection = collection
+    this.itemsPerPage = itemsPerPage
+	}
+	itemCount() {
+	// returns the number of items within the entire collection
+    return this.collection.length
+	}
+	pageCount() {
+	// returns the number of pages
+    return Math.ceil(this.collection.length / this.itemsPerPage)
+	}
+	pageItemCount(pageIndex) {
+	// returns the number of items on the current page. page_index is zero based.
+	// this method should return -1 for pageIndex values that are out of range
+    if (pageIndex + 1 > this.pageCount() || pageIndex < 0) return -1
+    else if (pageIndex + 1 === this.pageCount()) {
+      if (this.collection.length % this.itemsPerPage === 0) return this.itemsPerPage
+      else return this.collection.length % this.itemsPerPage
+    }
+    else return this.itemsPerPage
+	}
+	pageIndex(itemIndex) {
+	// determines what page an item is on. Zero based indexes
+	// this method should return -1 for itemIndex values that are out of range
+    if (itemIndex + 1 > this.collection.length || itemIndex < 0) return -1
+    return Math.ceil((itemIndex + 1)/this.itemsPerPage) - 1
+	}
+}
+
+// task 2 https://www.codewars.com/kata/52597aa56021e91c93000cb0
+const moveZeros = arr => {
+  let helper = arr.filter(el => el !== 0)
+  const n = arr.length - helper.length
+  for (let i = 0; i < n; i++) helper.push(0)
+  return helper
+}
+
+// task 3 https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3
+const findUniq2 = arr => {
+  let letters = [], first = arr[0], counter = 0, diff
+  for (let i = 0; i < first.length; i++) {
+    if (!letters.find(el => el.toLowerCase() === first[i].toLowerCase())) letters.push(first[i].toLowerCase())
+  }
+  for (let i = 1; i < arr.length; i++) {
+    for (j = 0; j < arr[i].length; j++) {
+      if (!letters.find(el => el.toLowerCase() === arr[i][j].toLowerCase())) {
+        if (i > 1 && counter === 0) return arr[i]
+        if (i > 1 && counter > 1) return arr[0]
+        diff = arr[i]
+        console.log(diff)
+        counter++
+      }
+    }
+    if (counter === 1 && i === 2 && diff ) return diff
+  }
+}
+
+// task 4 https://www.codewars.com/kata/5296bc77afba8baa690002d7
+const sudoku = puzzle => {
+  const isValid = (board, row, col, k) => {
+    for (let i = 0; i < 9; i++) {
+      const m = 3 * Math.floor(row / 3) + Math.floor(i / 3)
+      const n = 3 * Math.floor(col / 3) + i % 3
+      if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+        return false
+      }
+    }
+    return true
+  }
+  
+  const sudokuSolver = data => {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (data[i][j] == 0) {
+          for (let k = 1; k <= 9; k++) {
+            if (isValid(data, i, j, k)) {
+              data[i][j] = k
+              if (sudokuSolver(data)) return true
+              else data[i][j] = 0
+            }
+          }
+          return false
+        }
+      }
+    }
+    return true
+  }
+  sudokuSolver(puzzle)
+  return puzzle
+}
