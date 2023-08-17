d0911ff2977bba7de6088fb3e3354c17495bd124
Pavel Didenko <patriot2006_06@mail.ru>
2023-08-17 13:55:12 +0300

    fix: TypeError that appeared when the text inside the cells selected. fix bug with multiple selection of cells

--- homeworks/pavel-didenko/11-js-dom/js/index.js
+++ homeworks/pavel-didenko/11-js-dom/js/index.js
@@ -15,8 +15,8 @@ function createCell(coll, row) {
 function fillGrid(colls, rows, grid) {
   const cellNumber = colls * rows;
   for (let i = 0; i < cellNumber; i++) {
-    let rowsCount = Math.floor(i / colls) + 1;
-    let collsCount = (i % colls) + 1;
+    const rowsCount = Math.floor(i / colls) + 1;
+    const collsCount = (i % colls) + 1;
     grid.appendChild(createCell(collsCount, rowsCount));
   }
 }
@@ -24,26 +24,31 @@ function fillGrid(colls, rows, grid) {
 fillGrid(colls, rows, grid);
 
 grid.addEventListener('click', function (event) {
-  const selectedCell =
-    event.target.tagName === 'DIV' ? event.target : event.target.parentNode;
-  const column = selectedCell.firstChild.innerHTML.split('/')[0];
-  const row = selectedCell.firstChild.innerHTML.split('/')[1];
-  const selectedCellColor = 'rgb(0, 65, 169)';
-  const selectedRowColumnColor = 'aqua';
+  try {
+    const selectedCell =
+      event.target.tagName === 'DIV' ? event.target : event.target.parentNode;
+    if (selectedCell.className !== 'cells__wrapper') {
+      const column = selectedCell.firstChild.innerHTML.split('/')[0];
+      const row = selectedCell.firstChild.innerHTML.split('/')[1];
+      const selectedCellColor = 'rgb(0, 65, 169)';
+      const selectedRowColumnColor = 'aqua';
 
-  for (let child of this.children) {
-    if (
-      child.firstChild.innerHTML.split('/')[0] === column ||
-      child.firstChild.innerHTML.split('/')[1] === row
-    ) {
-      child.style.backgroundColor = selectedRowColumnColor;
-      child.firstChild.style.display = 'block';
-    } else if (!event.shiftKey) {
-      child.style.backgroundColor = '#FFFFFF';
-      child.firstChild.style.display = 'none';
+      for (let child of this.children) {
+        if (
+          child.firstChild.innerHTML.split('/')[0] === column ||
+          (child.firstChild.innerHTML.split('/')[1] === row && !event.shiftKey)
+        ) {
+          child.style.backgroundColor = selectedRowColumnColor;
+          child.firstChild.style.display = 'block';
+          selectedCell.style.backgroundColor = selectedCellColor;
+          selectedCell.firstChild.style.display = 'block';
+        } else if (!event.shiftKey) {
+          child.style.backgroundColor = '#FFFFFF';
+          child.firstChild.style.display = 'none';
+        }
+      }
     }
+  } catch (err) {
+    console.log(err);
   }
-
-  selectedCell.style.backgroundColor = selectedCellColor;
-  selectedCell.firstChild.style.display = 'block';
 });
