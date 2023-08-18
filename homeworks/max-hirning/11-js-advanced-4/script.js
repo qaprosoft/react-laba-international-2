e35e58aeb10d3e32c6a9eaad8d195b4c7366de36
Max Hirning <maxhirning25@gmail.com>
2023-08-18 15:26:10 +0300

    Fixed code: wrote event listener in Event Delegation

--- homeworks/max-hirning/11-js-advanced-4/script.js
+++ homeworks/max-hirning/11-js-advanced-4/script.js
@@ -1,26 +1,36 @@
+const numRows = 30;
+const numCols = 20;
 let selectedCells = new Set();
 const tbody = document.querySelector('tbody');
 
-drawGrid(30, 20);
+drawGrid();
 
-function drawGrid(numRows, numCols) {
+function drawGrid() {
   for (let i = 0; i < numRows; i++) {
     const row = document.createElement('tr');
     for (let j = 0; j < numCols; j++) {
       const cell = document.createElement('td');
       cell.setAttribute('data-row', i);
       cell.setAttribute('data-col', j);
-      cell.addEventListener('click', event =>
-        onBlockClick({event, j, i, numRows, numCols}),
-      );
       row.appendChild(cell);
     }
     tbody.appendChild(row);
   }
 }
 
-function onBlockClick({event, j, i, numRows, numCols}) {
+tbody.addEventListener('click', function (event) {
   const clickedCell = event.target;
+
+  if (clickedCell.tagName === 'TD') {
+    onBlockClick(event);
+  }
+});
+
+function onBlockClick(event) {
+  const clickedCell = event.target;
+  const blockColNum = clickedCell.getAttribute('data-col');
+  const blockRowNum = clickedCell.getAttribute('data-row');
+
   if (event.shiftKey) {
     selectedCells.add(clickedCell);
   } else {
@@ -32,7 +42,7 @@ function onBlockClick({event, j, i, numRows, numCols}) {
     selectedCells.add(clickedCell);
   }
   clickedCell.classList.add('selected');
-  clickedCell.textContent = `${j + 1}/${i + 1}`;
+  clickedCell.textContent = `${blockColNum + 1}/${blockRowNum + 1}`;
   for (const entry of selectedCells.entries()) {
     updateActiveRowCol(entry[0], numRows, numCols, event.shiftKey);
   }
