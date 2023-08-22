9f03e6907d96fdc4940e3637764a29529e5c98e6
Oleg Gorodenskyi <ogorodenskyi.laba@solvd.com>
2023-08-22 20:07:05 +0300

    added extra task, mad some changes and created log file

--- homeworks/oleg-gorodenskyi/12-data-structures-and-algorithms/solution.js
+++ homeworks/oleg-gorodenskyi/12-data-structures-and-algorithms/solution.js
@@ -1,8 +1,9 @@
 const data = require('./MOCK_DATA');
 const needleList = require('./needleList');
 const {performance} = require('perf_hooks');
+const fs = require('fs')
 
-// implementation checks and return true, if there are needleList array keys, in data array
+// implementation checks and return true, if there are needleList array keys, in data array and false, if there aren't.
 
 function straightSearch(needleList, data) {
   let count = 0;
@@ -20,20 +21,19 @@ function straightSearch(needleList, data) {
 }
 
 function binarySearch(needleList, data) {
-  const sortedData = [...data].sort((a, b) => a.sku.localeCompare(b.sku));
   let count = 0;
 
   for (const sku of needleList) {
     let leftIdx = 0;
-    let rightIdx = sortedData.length - 1;
+    let rightIdx = data.length - 1;
     let middleIdx;
 
     while (leftIdx <= rightIdx) {
       middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-      if (sku === sortedData[middleIdx].sku) {
+      if (sku === data[middleIdx].sku) {
         count += 1;
         break;
-      } else if (sku < sortedData[middleIdx].sku) {
+      } else if (sku < data[middleIdx].sku) {
         rightIdx = middleIdx - 1;
       } else {
         leftIdx = middleIdx + 1;
@@ -51,8 +51,43 @@ function measureTime(func, ...args) {
   return endTime;
 }
 
-const straightSearchTime = measureTime(straightSearch, needleList, data);
-console.log(`Straight search ${straightSearchTime} ms`);
+// Extra task
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-const binarySearchTime = measureTime(binarySearch, needleList, data);
-console.log(`Binary search ${binarySearchTime} ms`);
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const leftSide = [];
+  const rightSide = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftSide.push(arr[i]);
+    } else if (arr[i] > pivot) {
+      rightSide.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(leftSide), pivot, ...quickSort(rightSide)];
+}
+
+const sortedData = quickSort(data);
+const straightSearchTime = measureTime(straightSearch, needleList, data);
+console.log(`Straight search ${straightSearchTime} ms`);            
+
+const binarySearchTime = measureTime(binarySearch, needleList, sortedData);
+console.log(`Binary search ${binarySearchTime} ms`);                          
+
+const logContent = `
+Straight search ${straightSearchTime} ms
+Binary search ${binarySearchTime} ms
+`;
+
+fs.writeFile('result.log', logContent, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    `File successfully written`
+  }
+})
