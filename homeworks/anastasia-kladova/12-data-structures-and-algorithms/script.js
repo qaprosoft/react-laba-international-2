a2bff193993973c986f50c795677abef32d2208a
Nastya Kladova <nstsologub@gmail.com>
2023-08-22 20:13:20 +0200

    feat: add linear and binary search

---
+++ homeworks/anastasia-kladova/12-data-structures-and-algorithms/script.js
@@ -0,0 +1,29 @@
+const data = require('./data/MOCK_DATA');
+const needleList = require('./data/NEEDLE_LIST');
+
+const skuToFindReal = 'ccdb70f4-91f1-4543-93fa-8a93f980dc99';
+const skuToFindNoReal = 'ccccccccccccccccccccc';
+
+//Linear search
+const linearSearch = (array, sku) => {
+  const item = array.find(el => el.sku === sku);
+  return item ? item : 'The item not found';
+};
+
+//Binary search
+const sortedArray = data.sort((a, b) => a - b);
+const dataLength = sortedArray.length;
+
+const binarySearch = (array, sku) => {
+  let [start, end] = [0, dataLength - 1];
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+
+    if (array[middle].sku === sku) return array[middle];
+
+    array[middle].sku > sku ? (start = middle + 1) : (end = middle - 1);
+  }
+
+  return 'The item not found';
+};
