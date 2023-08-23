087ced8f93aaa442579177c4327baff65debbb52
Nastya Kladova <nstsologub@gmail.com>
2023-08-23 23:30:23 +0200

    docs: add webpack config

--- homeworks/anastasia-kladova/13-environment-and-preprocessors/webpack.config.js
+++ homeworks/anastasia-kladova/13-environment-and-preprocessors/webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path');
+const webPack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/js/script.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/public',
+    filename: 'script.js',
   },
+  devServer: {
+    port: 5000,
+    static: './public',
+    hot: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  devtool: 'source-map',
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: './src/assets/icons', to: 'assets/icons'},
+        {from: './src/assets/icons/social', to: 'assets/icons/social'},
+        {from: './src/assets/images', to: 'assets/images'},
+      ],
+    }),
+  ],
 };
