874271d277f49b26bc139bfddf9dac772727734d
Nastya Kladova <nstsologub@gmail.com>
2023-08-27 20:31:47 +0200

    feat: add examples

--- homeworks/anastasia-kladova/09-js-dvanced-2/script.js
+++ homeworks/anastasia-kladova/09-js-dvanced-2/script.js
@@ -91,3 +91,72 @@ function wakeUpProp(object) {
     }
   }
 }
+
+//examples
+class UserDTO extends Serializable {
+  constructor({firstName, lastName, phone, birth} = {}) {
+    super();
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.birth = birth;
+  }
+
+  printInfo() {
+    console.log(
+      `${this.firstName[0]}. ${this.lastName} - ${
+        this.phone
+      }, ${this.birth.toISOString()}`,
+    );
+  }
+}
+
+let tolik = new UserDTO({
+  firstName: 'Anatoliy',
+  lastName: 'Nashovich',
+  phone: '2020327',
+  birth: new Date('1999-01-02'),
+});
+
+tolik.printInfo(); //A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
+
+const serialized = tolik.serialize();
+tolik = null;
+
+const resurrectedTolik = new UserDTO().wakeFrom(serialized);
+
+console.log(resurrectedTolik instanceof UserDTO); // true
+console.log(resurrectedTolik.printInfo()); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
+
+class Post extends Serializable {
+  constructor({content, date, author} = {}) {
+    super();
+
+    this.content = content;
+    this.date = date;
+    this.author = author;
+  }
+}
+
+//console.log(new Post().wakeFrom(serialized));
+// throw an error because the srialized line does contain data for User class
+
+const testsPost = new Post({
+  content: {
+    number: 1,
+    null: null,
+    nan: NaN,
+    infinity: Infinity,
+    negInfinity: -Infinity,
+    negZero: -0,
+  },
+  date: new Date('2023-08-27'),
+  author: ['Anastasia', 'Kladova'],
+});
+
+const serializedTestsPost = testsPost.serialize();
+const wakeUpedTestsPost = new Post().wakeFrom(serializedTestsPost);
+
+console.log(serializedTestsPost);
+console.log(wakeUpedTestsPost);
