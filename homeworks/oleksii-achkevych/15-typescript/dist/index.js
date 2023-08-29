6321b754399ca20beb491f4c5befec9887cedcdc
ste1nerr <oachkevych@gmail.com>
2023-08-29 21:48:47 +0300

    done all tasks

---
+++ homeworks/oleksii-achkevych/15-typescript/dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+class BasicCalculator {
+    add(num1, num2) {
+        return { value: num1 + num2, error: '' };
+    }
+    subtract(num1, num2) {
+        return { value: num1 - num2, error: '' };
+    }
+    multiply(num1, num2) {
+        return { value: num1 * num2, error: '' };
+    }
+    divide(num1, num2) {
+        if (num2 === 0) {
+            return { value: 0, error: 'division by zero' };
+        }
+        return { value: num1 / num2, error: '' };
+    }
+    power(base, exponent) {
+        if (exponent <= 0 || !Number.isInteger(exponent)) {
+            return { value: 0, error: 'must be a positive integer' };
+        }
+        return { value: Math.pow(base, exponent), error: '' };
+    }
+    sqrt(num) {
+        if (num < 0) {
+            return { value: 0, error: 'negative number' };
+        }
+        return { value: Math.sqrt(num), error: '' };
+    }
+}
+const calculator = new BasicCalculator();
+console.log(calculator.add(5, 3));
+console.log(calculator.subtract(5, 3));
+console.log(calculator.multiply(5, 3));
+console.log(calculator.divide(5, 0));
+console.log(calculator.divide(5, 3));
+console.log(calculator.power(2, 3));
+console.log(calculator.power(2, -1));
+console.log(calculator.sqrt(16));
+console.log(calculator.sqrt(-16));
+//# sourceMappingURL=index.js.map
