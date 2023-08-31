6d19852604e7ee9a94490840dc2e3d924aaaf096
carlon702 <carlosj.marchal@gmail.com>
2023-08-31 12:37:50 -0300

    feat: added script and color change functionality

--- homeworks/carlos-marchal/11-js-dom/script.js
+++ homeworks/carlos-marchal/11-js-dom/script.js
@@ -1,15 +1,20 @@
 const gridContainer = document.querySelector(".grid");
 const rows = 30;
 const columns = 20;
+let selectedCell;
+let selectedRow;
+let selectedColumn;
 
 
 function createGrid(){
-for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid_cell');
-        gridItem.textContent = `y=${row + 1}, x=${col + 1}`; // You can customize the content here
-        gridContainer.appendChild(gridItem);
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < columns; col++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid__cell');
+        gridCell.textContent = `y=${row + 1}, x=${col + 1}`;
+        gridCell.setAttribute("row", `${row+1}`);
+        gridCell.setAttribute("column", `${col+1}`);
+        gridContainer.appendChild(gridCell);
     }
 }
 }
@@ -18,3 +23,44 @@ createGrid();
 
 
 
+const cells = document.querySelectorAll(".grid__cell");
+
+cells.forEach(e => {
+    e.addEventListener("click", ()=>{
+        if(selectedCell === undefined){
+            toggleSelection(e, e.getAttribute("row"), e.getAttribute("column"))
+        } else if( selectedCell === e){
+            toggleSelection(selectedCell, selectedRow, selectedColumn);
+        }
+        toggleSelection(selectedCell, selectedRow, selectedColumn)
+      
+        toggleSelection(e, e.getAttribute("row"), e.getAttribute("column"));
+        
+    })
+});
+
+
+
+
+function getCellsByRowOrCol(axis, value){
+    const cells = document.querySelectorAll(".grid__cell");
+    const resultArr = [];
+    cells.forEach(e=>{e.getAttribute(axis)==value? resultArr.push(e):null});
+    return resultArr;
+};
+
+function toggleSelection(element, row, column){
+    getCellsByRowOrCol("row", row).forEach(e=>e.getAttribute("column")!=column? e.classList.toggle("grid__cell--secondary"): null);
+    getCellsByRowOrCol("column", column).forEach(e=>e.getAttribute("row")!=row? e.classList.toggle("grid__cell--secondary"): null);
+    element.classList.toggle("grid__cell--selected");
+
+    if(element.classList.contains("grid__cell--selected")){
+        selectedRow = element.getAttribute("row");
+        selectedColumn = element.getAttribute("column");
+        selectedCell = element;
+    } else {
+        selectedCell = undefined;
+        selectedRow = undefined;
+        selectedColumn = undefined;
+    }
+}
