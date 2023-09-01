8e645fc3c832c0da3f65e22593a63f4ce6ecd622
stkrolikiewicz <st.krolikiewicz@gmail.com>
2023-09-01 15:02:14 +0200

    fix: add 'active' class to active row/colum & handle click only for instances of table cell

--- homeworks/stanislaw-krolikiewicz/11-js-dom/scripts/grid.js
+++ homeworks/stanislaw-krolikiewicz/11-js-dom/scripts/grid.js
@@ -14,16 +14,28 @@ for (let i = 0; i < 30; i++) {
 }
 
 const handleClick = e => {
-  const prevActiveCells = grid.querySelectorAll('td.active');
-  if (prevActiveCells.length > 0) {
-    prevActiveCells.forEach(element => {
-      element.classList.remove('active');
-      element.innerText = '';
+  if (e.target instanceof HTMLTableCellElement) {
+    const prevActiveCells = grid.querySelectorAll('td.active');
+    const prevActiveRow = grid.querySelector('tr.active');
+    if (prevActiveCells.length > 0) {
+      prevActiveCells.forEach(element => {
+        element.classList.remove('active');
+        element.innerText = '';
+      });
+      prevActiveRow.classList.remove('active');
+    }
+    const activeCell = e.target;
+    const {row, col} = activeCell.dataset;
+    const activeRow = grid.querySelector(`tr[data-row="${row}"]`);
+    const activeColumnCells = grid.querySelectorAll(`td[data-col="${col}"]`);
+    activeCell.classList.add('active');
+    activeColumnCells.forEach(element => {
+      element.classList.add('active');
     });
+    activeRow.classList.add('active');
+    activeCell.innerText =
+      activeCell.dataset.col + '/' + activeCell.dataset.row;
   }
-  const activeCell = e.target;
-  activeCell.classList.add('active');
-  activeCell.innerText = activeCell.dataset.col + '/' + activeCell.dataset.row;
 };
 
 grid.addEventListener('click', handleClick);
