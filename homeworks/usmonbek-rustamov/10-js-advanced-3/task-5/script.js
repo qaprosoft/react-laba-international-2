8aa505a51aadb2aad4cf44d26d229895e0e7661f
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-09-05 01:50:19 +0200

    utilize fetch request & create users

---
+++ homeworks/usmonbek-rustamov/10-js-advanced-3/task-5/script.js
@@ -0,0 +1,44 @@
+import fetchRequest from './requests/fetch.js';
+import xmlhttpRequest from './requests/xmlhttp.js';
+
+// Elements
+const requestForm = document.querySelector('.users__request-form');
+const usersList = document.querySelector('.users__user-list');
+
+const BASE_URL = 'https://randomuser.me/api/';
+const requestFunc = {
+  fetch: fetchRequest,
+  xmlhttp: xmlhttpRequest,
+};
+
+requestForm.addEventListener('submit', generateUsers);
+
+async function generateUsers(e) {
+  e.preventDefault();
+
+  const requestType = document.querySelector('#request').value;
+  const gender = document.querySelector('#gender').value;
+  const usersCount = document.querySelector('#users-count').value;
+
+  if (usersCount < 1) return;
+
+  usersList.innerHTML = '<p class="users__loader">Loading...</p>';
+  const {results} = await requestFunc[requestType](
+    BASE_URL,
+    gender,
+    usersCount,
+  );
+
+  usersList.innerHTML = '';
+  results.forEach(user => {
+    const fullName = `${user.name.first} ${user.name.last}`;
+    const userItem = `
+      <li class='users__user-item'>
+        <img src=${user.picture.medium} alt=${fullName} />
+        <p>${fullName}</p>
+      </li>
+      `;
+
+    usersList.insertAdjacentHTML('beforeend', userItem);
+  });
+}
