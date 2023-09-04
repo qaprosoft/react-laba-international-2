0b5b0b94448aa9c5aafcc22a33fd9256554e0fb8
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-09-04 14:23:09 +0300

    formatted  the code and fixed compiling code in scss by importing blocks directly to the js file

--- homeworks/oleg-gorodenskyi/13-environment-and-preprocessors/js/script.js
+++ homeworks/oleg-gorodenskyi/13-environment-and-preprocessors/js/script.js
@@ -1,42 +1,50 @@
-import '../src/style.scss'
+import '../src/base/animation.scss'
 
-window.addEventListener("DOMContentLoaded", function() {
+import '../src/blocks/media.scss'
 
-    const tabs = document.querySelectorAll('.tabs__item');
-    const cards = document.querySelectorAll('.portfolio__card');
-    const pageup = document.querySelector('.pageup');
+import '../src/blocks/profile.scss'
+import '../src/blocks/tabs.scss'
+import '../src/blocks/portfolio.scss'
+import '../src/blocks/footer.scss'
 
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener("click", function() {
-        for (let j = 0; j < tabs.length; j++) {
-          tabs[j].classList.remove("active");
-        }
-        this.classList.add("active");
-      });
-    }
+import '../src/style.scss';
 
-    function handleScroll() {
-      cards.forEach(function(card) {
-        const cardTop = card.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-    
-        if (cardTop < windowHeight) {
-          card.classList.add('show');
-        }
-      });
-    }
+window.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll('.tabs__item');
+  const cards = document.querySelectorAll('.portfolio__card');
+  const pageup = document.querySelector('.pageup');
+  const btn = document.querySelector('.profile__buttons-btn-download');
 
-    function scrollToTop() {
-      if (window.scrollY > 300 && window.innerWidth <= 599) {
-        pageup.classList.add('show');
-      } else {
-        pageup.classList.remove('show');
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', function () {
+      for (let j = 0; j < tabs.length; j++) {
+        tabs[j].classList.remove('active');
       }
+      this.classList.add('active');
+    });
+  }
+
+  function handleScroll() {
+    cards.forEach(function (card) {
+      const cardTop = card.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (cardTop < windowHeight) {
+        card.classList.add('show');
+      }
+    });
+  }
+
+  function scrollToTop() {
+    console.log(window.innerWidth);
+    if (window.scrollY > 300 && window.innerWidth <= 599) {
+      console.log(window.scrollY);
+      pageup.classList.add('show');
+    } else {
+      pageup.classList.remove('show');
     }
+  }
 
-    this.window.addEventListener('scroll', handleScroll)
-    this.window.addEventListener('scroll', scrollToTop)
-
-  });
-  
-
+  this.window.addEventListener('scroll', handleScroll);
+  this.window.addEventListener('scroll', scrollToTop);
+});
