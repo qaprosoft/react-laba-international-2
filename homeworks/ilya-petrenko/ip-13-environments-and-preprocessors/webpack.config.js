534a2191a5c4310b3c790954d5957cff5a9498f5
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-09-04 11:20:53 +0300

    Finished HW13

---
+++ homeworks/ilya-petrenko/ip-13-environments-and-preprocessors/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: './script.js',
+    styles: './src/styles/main.scss',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
+
