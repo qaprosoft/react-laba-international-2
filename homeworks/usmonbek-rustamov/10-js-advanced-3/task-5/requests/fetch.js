1ef6609437fdc3a80582e79aeb6844074d076c9a
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-09-05 02:15:35 +0200

    finish task 5

--- homeworks/usmonbek-rustamov/10-js-advanced-3/task-5/requests/fetch.js
+++ homeworks/usmonbek-rustamov/10-js-advanced-3/task-5/requests/fetch.js
@@ -1,17 +1,13 @@
 const fetchRequest = async (baseUrl, gender, usersCount) => {
-  try {
-    const response = await fetch(
-      `${baseUrl}?gender=${gender}&results=${usersCount}`,
-    );
+  const response = await fetch(
+    `${baseUrl}?gender=${gender}&results=${usersCount}`,
+  );
 
-    if (!response.ok) {
-      throw Error(`HTTP status code: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (err) {
-    console.log(err);
+  if (!response.ok) {
+    throw Error(`HTTP status code: ${response.status}`);
   }
+
+  return await response.json();
 };
 
 export default fetchRequest;
--- homeworks/usmonbek-rustamov/10-js-advanced-3/task-5/script.js
+++ homeworks/usmonbek-rustamov/10-js-advanced-3/task-5/script.js
@@ -22,23 +22,27 @@ async function generateUsers(e) {
 
   if (usersCount < 1) return;
 
-  usersList.innerHTML = '<p class="users__loader">Loading...</p>';
-  const {results} = await requestFunc[requestType](
-    BASE_URL,
-    gender,
-    usersCount,
-  );
+  try {
+    usersList.innerHTML = '<p class="users__loader">Loading...</p>';
+    const {results} = await requestFunc[requestType](
+      BASE_URL,
+      gender,
+      usersCount,
+    );
 
-  usersList.innerHTML = '';
-  results.forEach(user => {
-    const fullName = `${user.name.first} ${user.name.last}`;
-    const userItem = `
+    usersList.innerHTML = '';
+    results.forEach(user => {
+      const fullName = `${user.name.first} ${user.name.last}`;
+      const userItem = `
       <li class='users__user-item'>
-        <img src=${user.picture.medium} alt=${fullName} />
-        <p>${fullName}</p>
+      <img src=${user.picture.medium} alt=${fullName} />
+      <p>${fullName}</p>
       </li>
       `;
 
-    usersList.insertAdjacentHTML('beforeend', userItem);
-  });
+      usersList.insertAdjacentHTML('beforeend', userItem);
+    });
+  } catch (err) {
+    usersList.innerHTML = `<p class="users__loader">${err}</p>`;
+  }
 }
