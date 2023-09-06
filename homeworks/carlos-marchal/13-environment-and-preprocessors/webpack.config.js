6d97dda65d3576584b7397dbad1cb788bbc5db74
carlon702 <carlosj.marchal@gmail.com>
2023-09-06 14:28:56 -0300

    fix: format added

--- homeworks/carlos-marchal/13-environment-and-preprocessors/webpack.config.js
+++ homeworks/carlos-marchal/13-environment-and-preprocessors/webpack.config.js
@@ -1,34 +1,24 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        port:5000, 
-        static: path.join(__dirname,"./src"),
-        hot: true, 
-        open: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader',
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'compressedStyles.css',
-      }),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
-  };
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'compressedStyles.css',
+    }),
+  ],
+};
