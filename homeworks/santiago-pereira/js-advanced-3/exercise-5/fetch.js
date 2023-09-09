cdbb67a87e7c3df44ebd744297784f6d976b150d
Santiago-Pereira <santiagopereira_8@hotmail.com>
2023-09-09 20:17:58 -0300

    "fixing formatting and merging problems"

--- homeworks/santiago-pereira/js-advanced-3/exercise-5/fetch.js
+++ homeworks/santiago-pereira/js-advanced-3/exercise-5/fetch.js
@@ -1,19 +1,19 @@
 //selector:
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 const getData = async url => {
-    const data = await fetch(url);
-    const jsonData = await data.json();
-  
-    const showData = async people => {
-      people.results.map(person => {
-        container.innerHTML += `<div class="runner-container"><img src="${person.picture.thumbnail}">
+  const data = await fetch(url);
+  const jsonData = await data.json();
+
+  const showData = async people => {
+    people.results.map(person => {
+      container.innerHTML += `<div class="runner-container"><img src="${person.picture.thumbnail}">
       </img>
       <h3>${person.name.title} ${person.name.first} ${person.name.last}</h3></div>`;
-      });
-    };
-  
-    showData(jsonData);
+    });
   };
-  
-  getData('https://randomuser.me/api/?results=10');
+
+  showData(jsonData);
+};
+
+getData('https://randomuser.me/api/?results=10');
