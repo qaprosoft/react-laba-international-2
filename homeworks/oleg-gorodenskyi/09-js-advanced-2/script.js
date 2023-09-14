07beff2b201f85b130503569acc7c4d24efa2abf
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-09-14 23:16:28 +0300

    added hasInfinityOrNaN method to check NaN and Infinity during serialization

--- homeworks/oleg-gorodenskyi/09-js-advanced-2/script.js
+++ homeworks/oleg-gorodenskyi/09-js-advanced-2/script.js
@@ -19,29 +19,42 @@ class Serializable {
         }
       }
     }
-    return JSON.stringify(serialized);
+    return JSON.stringify(serialized, this.hasInfinityOrNaN);
   }
 
   wakeFrom(serialized) {
     const serializedObj = JSON.parse(serialized);
-    return new this.constructor(serializedObj);
+    const instance = new this.constructor(serializedObj);
+    if (serializedObj.hasOwnProperty('birth') && typeof serializedObj.birth === 'string') {
+      instance.birth = new Date(serializedObj.birth);
+    }
+    return instance
+  }
+
+  hasInfinityOrNaN(key, value) {
+    if (value === Infinity) {
+      return 'Infinity'
+    } else if (Number.isNaN(value)) {
+      return 'NaN';
+    } else if (value === -Infinity) {
+      return '-Infinity'
+    } return value
   }
 }
 
 class UserDTO extends Serializable {
-  constructor({firstName, lastName, phone, birth}) {
+  constructor({ firstName, lastName, phone, birth }) {
     super();
 
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
-    this.birth = new Date(birth);
+    this.birth = birth;
   }
 
   printInfo() {
     console.log(
-      `${this.firstName[0]}. ${this.lastName} - ${
-        this.phone
+      `${this.firstName[0]}. ${this.lastName} - ${this.phone
       }, ${this.birth.toISOString()}`,
     );
   }
@@ -54,16 +67,30 @@ let tolik = new UserDTO({
   birth: new Date('1999-01-02'),
 });
 
-const serialized = tolik.serialize();
-tolik = null;
+let oleg = new UserDTO({
+  firstName: "Oleg",
+  lastName: Infinity,
+  phone: -5 / 0,
+  birth: null,
+})
 
-const resurrectedTolik = new UserDTO({}).wakeFrom(serialized);
 
-console.log(resurrectedTolik instanceof UserDTO);
-resurrectedTolik.printInfo();
+
+const serialized = oleg.serialize();
+console.log(serialized)
+
+
+// const serialized = tolik.serialize();
+// console.log(serialized)
+// tolik = null;
+
+// const resurrectedTolik = new UserDTO({}).wakeFrom(serialized);
+
+// console.log(resurrectedTolik instanceof UserDTO);
+// resurrectedTolik.printInfo();
 
 class Post extends Serializable {
-  constructor({content, date, author}) {
+  constructor({ content, date, author }) {
     super();
 
     this.content = content;
