ed45e9b5bad1890e19d27df0c01e306a769c9827
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-09-15 15:33:40 +0300

    added customReviver method to parse data correctly

--- homeworks/oleg-gorodenskyi/09-js-advanced-2/script.js
+++ homeworks/oleg-gorodenskyi/09-js-advanced-2/script.js
@@ -23,27 +23,38 @@ class Serializable {
   }
 
   wakeFrom(serialized) {
-    const serializedObj = JSON.parse(serialized);
+    const serializedObj = JSON.parse(serialized, this.customReviver);
     const instance = new this.constructor(serializedObj);
-    if (serializedObj.hasOwnProperty('birth') && typeof serializedObj.birth === 'string') {
-      instance.birth = new Date(serializedObj.birth);
-    }
-    return instance
+    return instance;
   }
 
   hasInfinityOrNaN(key, value) {
     if (value === Infinity) {
-      return 'Infinity'
+      return 'Infinity';
     } else if (Number.isNaN(value)) {
       return 'NaN';
     } else if (value === -Infinity) {
-      return '-Infinity'
-    } return value
+      return '-Infinity';
+    }
+    return value;
+  }
+
+  customReviver(key, value) {
+    if (
+      typeof value === 'string' &&
+      /^\d{4}-[01]\d-[0-3]\dT[012]\d(?::[0-6]\d){2}\.\d{3}Z$/.test(value)
+    ) {
+      const correctParsedDate = new Date(value);
+      return correctParsedDate;
+    } else if (typeof value === 'string' && value === 'Infinity') {
+      value = Infinity;
+    }
+    return value;
   }
 }
 
 class UserDTO extends Serializable {
-  constructor({ firstName, lastName, phone, birth }) {
+  constructor({firstName, lastName, phone, birth}) {
     super();
 
     this.firstName = firstName;
@@ -54,7 +65,8 @@ class UserDTO extends Serializable {
 
   printInfo() {
     console.log(
-      `${this.firstName[0]}. ${this.lastName} - ${this.phone
+      `${this.firstName[0]}. ${this.lastName} - ${
+        this.phone
       }, ${this.birth.toISOString()}`,
     );
   }
@@ -68,16 +80,18 @@ let tolik = new UserDTO({
 });
 
 let oleg = new UserDTO({
-  firstName: "Oleg",
+  firstName: 'Oleg',
   lastName: Infinity,
   phone: -5 / 0,
   birth: null,
-})
-
-
+});
 
 const serialized = oleg.serialize();
-console.log(serialized)
+console.log(serialized);
+oleg = null;
+
+const resurrectedOleg = new UserDTO({}).wakeFrom(serialized);
+console.log(resurrectedOleg instanceof UserDTO);
 
 
 // const serialized = tolik.serialize();
@@ -87,10 +101,18 @@ console.log(serialized)
 // const resurrectedTolik = new UserDTO({}).wakeFrom(serialized);
 
 // console.log(resurrectedTolik instanceof UserDTO);
+
+// console.log(resurrectedTolik)
 // resurrectedTolik.printInfo();
 
 class Post extends Serializable {
-  constructor({ content, date, author }) {
+  constructor(
+    {content = '', date = '', author = ''} = {
+      content: '',
+      date: '',
+      author: '',
+    },
+  ) {
     super();
 
     this.content = content;
@@ -98,6 +120,8 @@ class Post extends Serializable {
     this.author = author;
   }
 }
+
+// console.log(new Post().wakeFrom(serialized));
 
 try {
   new Post({}).wakeFrom(serialized);
