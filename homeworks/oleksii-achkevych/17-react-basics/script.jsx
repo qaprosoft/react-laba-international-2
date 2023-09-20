65bd88b0d00a4052e67a3c34e4d99c176626cdf4
ste1nerr <oachkevych@gmail.com>
2023-09-21 01:28:51 +0300

    done all tasks

---
+++ homeworks/oleksii-achkevych/17-react-basics/script.jsx
@@ -0,0 +1,33 @@
+const rootContainer = document.querySelector('#root_container');
+
+const TrafficLight = () => {
+  const [currentColorIndex, setCurrentColorIndex] = React.useState(0);
+  const colors = ['RGB(223, 64, 64)', 'RGB(233, 236, 106)', 'RGB(4, 202, 0)'];
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return React.createElement(
+    'div',
+    { id: 'traffic-light' },
+    React.createElement('div', {
+      className: 'top-light',
+    }),
+    colors.map((color, index) =>
+      React.createElement('div', {
+        key: color,
+        className: `light ${currentColorIndex === index ? 'active' : ''}`,
+        style: {
+          background: currentColorIndex === index ? color : 'gray',
+        },
+      })
+    )
+  );
+}
+
+ReactDOM.render(React.createElement(TrafficLight), rootContainer);
