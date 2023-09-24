484e87062cc2d1c1b88ed9b93cdb368741681fb7
Pavel Didenko <patriot2006_06@mail.ru>
2023-09-24 16:27:25 +0300

    fix: errors related to same keys for different DOM elements

--- homeworks/pavel-didenko/18-react-ui/components/refreshUsers/RefreshAllUsers.jsx
+++ homeworks/pavel-didenko/18-react-ui/components/refreshUsers/RefreshAllUsers.jsx
@@ -1,17 +1,21 @@
-const RefreshAllUsers = ({setUsers, fetchUsers, allUsers}) => {
+const RefreshAllUsers = ({setUsers, fetchUsers, allUsers, setAllUsers, users}) => {
   async function refreshAll() {
     await fetchUsers();
     setUsers(users => {
       return users.map((_, index) => {
-        console.log(allUsers);
-        return {id: allUsers[index].id, url: allUsers[index].url};
-      });
+        if (index < allUsers.length){
+          return {id: allUsers[index].id, url: allUsers[index].url};
+        }
+      })
     });
-
   }
 
   return (
-    <button className="refresh-button" onClick={refreshAll}>
+    <button
+      className="refresh-button"
+      onClick={refreshAll}
+      style={{marginTop: users.length < 10 ? '20.9rem' : '3rem'}}
+    >
       Refresh All
     </button>
   );
