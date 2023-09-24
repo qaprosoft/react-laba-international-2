ae2e2cb24b781117ec28e4f7b9f02faf22b7d3f6
Pavel Didenko <patriot2006_06@mail.ru>
2023-09-24 03:21:42 +0300

    feat: implement adding random user foto to the site

--- homeworks/pavel-didenko/18-react-ui/index.jsx
+++ homeworks/pavel-didenko/18-react-ui/index.jsx
@@ -1,31 +1,44 @@
 const useState = React.useState;
+const useEffect = React.useEffect;
 
 const App = () => {
+  const [allUsers, setAllUsers] = useState([]);
   const [users, setUsers] = useState([]);
+  // const [usersFetched, setUsersFetched] = useState(false);
+
 
   async function fetchUsers() {
     const url = 'https://tinyfac.es/api/data?limit=50&quality=0';
-
     try {
       const response = await fetch(url);
       const json = await response.json();
-      return getRandomUser(json);
+      console.log(json)
+      setAllUsers(json);
     } catch (err) {
       console.log(err);
     }
   }
 
-  function getRandomUser(usersArray) {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+
+  function getRandomUser() {
     const min = 0;
-    const max = Math.floor(usersArray.length);
-    const user = usersArray[Math.floor(Math.random() * (max - min + 1) + min)];
+    const max = Math.floor(allUsers.length);
+    const user = allUsers[Math.floor(Math.random() * (max - min) + min)];
     return {id: user.id, foto: user.url};
   }
 
   async function addUserToState() {
-    const userObject = await fetchUsers();
-    setUsers(users.concat(userObject));
-    console.log(users);
+    if(allUsers.length > 0){
+      const userObject = getRandomUser();
+      setAllUsers(() => allUsers.filter(item => item.id !== userObject.id));
+      setUsers(users.concat(userObject));
+      console.log(users);
+      console.log(allUsers);
+    }
   }
 
   return (
