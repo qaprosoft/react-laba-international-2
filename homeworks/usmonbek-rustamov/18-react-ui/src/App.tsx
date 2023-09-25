23a46b8fc585c7e8babf935cd5921793f5af6d49
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-09-26 01:23:06 +0200

    setup project

---
+++ homeworks/usmonbek-rustamov/18-react-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import AvatarList from './components/AvatarList';
+
+function App() {
+  return (
+    <main className="main">
+      <div className="container">
+        <AvatarList />
+      </div>
+    </main>
+  );
+}
+
+export default App;
