ea2159988f1987a266663163363d838bb90a58c2
Rivenrem <rivensenik@gmail.com>
2023-09-25 19:31:22 +0300

    refactor: remove refreshButton component

--- homeworks/lizaveta-kryvanosava/18-react-ui/src/app/page.tsx
+++ homeworks/lizaveta-kryvanosava/18-react-ui/src/app/page.tsx
@@ -1,49 +1,78 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import styles from '@/app/page.module.scss';
 import AddAvatar from '@/components/AddAvatar';
 import Avatar from '@/components/Avatar';
-import RefreshButton from '@/components/RefreshButton';
 import getNewAvatars from '@/helpers/getNewAvatars';
-import updateAllAvatars from '@/helpers/updateAllAvatars';
 import IAvatarResponse from '@/types/avatarResponse';
 
 export default function Home() {
-  const [avatars, setAvatars] = useState<Array<IAvatarResponse>>([]);
-  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
+  const [avatars, setAvatars] = useState<IAvatarResponse[]>([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [refreshingIndex, setRefreshingIndex] = useState<number | null>(null);
 
   const addNewAvatar = async () => {
     try {
       setAvatars([...avatars, ...(await getNewAvatars(1))]);
-    } catch (error) {
-      console.log(error);
+    } catch {}
+  };
+
+  const refreshAvatar = useCallback(
+    async (avatarId: number) => {
+      try {
+        const index = avatars.findIndex(({ id }) => id == avatarId);
+
+        setRefreshingIndex(index);
+
+        const newAvatars = [...avatars];
+        newAvatars[index] = (await getNewAvatars(1))[0];
+
+        setAvatars(newAvatars);
+      } catch {
+      } finally {
+        setRefreshingIndex(null);
+      }
+    },
+    [avatars],
+  );
+
+  const updateAllAvatars = async () => {
+    if (!avatars.length) return;
+
+    setIsRefreshing(true);
+
+    try {
+      const newAvatars = await getNewAvatars(avatars.length);
+      setAvatars(newAvatars);
+    } catch {
+    } finally {
+      setIsRefreshing(false);
     }
   };
 
   return (
     <main className={styles.main}>
       <section className={styles.main__avatars}>
-        {avatars.map(avatar => {
-          return (
-            <Avatar
-              avatar={avatar}
-              isRefreshingAll={isRefreshing}
-              key={avatar.id}
-            />
-          );
-        })}
+        {avatars.map((avatar, index) => (
+          <Avatar
+            avatar={avatar}
+            isLoading={isRefreshing || refreshingIndex === index}
+            clickHandler={refreshAvatar}
+            key={avatar.id}
+          />
+        ))}
         <AddAvatar addNewAvatar={addNewAvatar} />
       </section>
 
       <section className={styles.main__refresh}>
-        <RefreshButton
-          text="Refresh All"
-          clickHandler={() => {
-            updateAllAvatars(avatars, setAvatars, setIsRefreshing);
-          }}
-        />
+        <button
+          onClick={updateAllAvatars}
+          className={styles['main__refresh-button']}
+        >
+          Refresh All
+        </button>
       </section>
     </main>
   );
