6075589e0ae7e9d9d7ee8f2551f2514ad7f66428
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-09-26 17:44:24 +0200

    throw error when receiving rejected promise

--- homeworks/usmonbek-rustamov/18-react-ui/src/http/avatars.ts
+++ homeworks/usmonbek-rustamov/18-react-ui/src/http/avatars.ts
@@ -3,14 +3,12 @@ import {AvatarData} from '../common/types';
 const BASE_URL: string = ' https://tinyfac.es/api';
 
 export const getAvatars = async (count: number): Promise<AvatarData[]> => {
-  try {
-    const avatarResponse = await fetch(
-      `${BASE_URL}/data?limit=${count}&quality=0`,
-    );
-    const avatarList = await avatarResponse.json();
+  const avatarResponse = await fetch(
+    `${BASE_URL}/data?limit=${count}&quality=0`,
+  );
+  const avatarList = await avatarResponse.json();
 
-    return avatarList;
-  } catch (error) {
-    throw new Error('Request failed');
-  }
+  if (avatarList.error) throw new Error(avatarList.reason);
+
+  return avatarList;
 };
