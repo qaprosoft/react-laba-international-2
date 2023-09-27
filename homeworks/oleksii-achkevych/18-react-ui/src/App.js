4e3d8f3018a264b2e6fc2a3fe458a16733cd1f90
ste1nerr <oachkevych@gmail.com>
2023-09-28 01:32:35 +0300

    done all tasks

--- homeworks/oleksii-achkevych/18-react-ui/src/App.js
+++ homeworks/oleksii-achkevych/18-react-ui/src/App.js
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
-import Avatar from './Components/Avatar/Avatar';
+import Avatar from '../src/сomponents/Avatar/Avatar';
+
+const API_URL = 'https://tinyfac.es/api/data';
 
 const App = () => {
   const [avatars, setAvatars] = useState([]);
+  const [addedAvatarsCount, setAddedAvatarsCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateButtonLoading, setIsUpdateButtonLoading] = useState(false);
+  const [isAddButtonLoading, setIsAddButtonLoading] = useState(false);
 
   useEffect(() => {
-    const fetchAvatars = async () => {
-      try {
-        const response = await axios.get(
-          'https://tinyfac.es/api/data?limit=20&quality=0'
-        );
-        setAvatars(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchAvatars();
   }, []);
 
-
-  const refreshAvatar = async (index) => {
+  const fetchAvatars = async () => {
+    setIsLoading(true);
     try {
       const response = await axios.get(
-        'https://tinyfac.es/api/data?limit=1&quality=0'
+        `${API_URL}?limit=${20 + addedAvatarsCount}&quality=0`,
       );
-      setAvatars((prevAvatars) => {
+      setAvatars(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const refreshAvatar = async index => {
+    try {
+      const response = await axios.get(`${API_URL}?limit=1&quality=0`);
+      setAvatars(prevAvatars => {
         const newAvatars = [...prevAvatars];
         newAvatars[index] = response.data[0];
         return newAvatars;
@@ -36,30 +43,32 @@ const App = () => {
     }
   };
 
-
   const refreshAllAvatars = async () => {
+    setIsLoading(true);
     try {
       const response = await axios.get(
-        'https://tinyfac.es/api/data?limit=20&quality=0'
+        `${API_URL}?limit=${20 + addedAvatarsCount}&quality=0`,
       );
       setAvatars(response.data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
-
 
   const addRandomAvatarTile = async () => {
+    setIsAddButtonLoading(true);
     try {
-      const response = await axios.get(
-        'https://tinyfac.es/api/data?limit=1&quality=0'
-      );
+      const response = await axios.get(`${API_URL}?limit=1&quality=0`);
       setAvatars([...avatars, response.data[0]]);
+      setAddedAvatarsCount(count => count + 1);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsAddButtonLoading(false);
     }
   };
-
 
   return (
     <div className="App">
@@ -75,14 +84,21 @@ const App = () => {
           <button
             className="empty-square-button"
             onClick={addRandomAvatarTile}
+            style={{display: isAddButtonLoading ? 'none' : 'flex'}}
           >
             +
           </button>
         )}
       </div>
-      <button onClick={refreshAllAvatars}>Refresh All</button>
+      <button
+        onClick={refreshAllAvatars}
+        disabled={isUpdateButtonLoading}
+        style={{display: isLoading ? 'none' : ''}}
+      >
+        Update
+      </button>
     </div>
   );
-}
+};
 
 export default App;
