cd9804b8fec0cd40be0f6dd30f40cf6f98e19948
Andrew <gandrejko2002@gmail.com>
2023-09-28 16:08:21 +0300

    + added modal

--- homeworks/andrew-kondratiyk/19-react-hooks/src/components/pages/main-page/MainPage.tsx
+++ homeworks/andrew-kondratiyk/19-react-hooks/src/components/pages/main-page/MainPage.tsx
@@ -1,16 +1,34 @@
-import EditTodoCard from '@/components/common/edit-todo-card/EditTodoCard';
+'use client';
+
+import Modal from '@/components/common/modal/Modal';
+import {useRouter} from 'next/navigation';
 import styles from './MainPage.module.css';
 const MainPage = () => {
+  const router = useRouter();
+  const isLoggedIn = false;
   return (
     <main className={styles.container}>
-      <div className={styles.header}>
-        <h1>Todos</h1>
-        <div className={styles.buttons}>
-          <button className={styles.button}>Clear finished todos</button>
-          <button className={styles.button}>Clear all todos</button>
-        </div>
-      </div>
-      <div className={styles.divider}></div>
+      {isLoggedIn ? (
+        <>
+          <div className={styles.header}>
+            <h1>Todos</h1>
+            <div className={styles.buttons}>
+              <button className={styles.button}>Clear finished todos</button>
+              <button className={styles.button}>Clear all todos</button>
+            </div>
+          </div>
+          <div className={styles.divider}></div>
+        </>
+      ) : (
+        <Modal isOpen={true} title="Please, login to see your todos">
+          <button
+            className={styles.siginButton}
+            onClick={() => router.push('/login')}
+          >
+            Sign in
+          </button>
+        </Modal>
+      )}
     </main>
   );
 };
