e79f6c8e8199fdc8fde181f174258acb6000a937
Pavel Didenko <patriot2006_06@mail.ru>
2023-09-28 18:02:53 +0300

    feat: implement minimum task length validation during task creation

--- homeworks/pavel-didenko/19-react-hooks/src/components/TaskCreator/TaskCreator.jsx
+++ homeworks/pavel-didenko/19-react-hooks/src/components/TaskCreator/TaskCreator.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './taskCreator.css';
-import { useRef } from 'react';
+import {useRef} from 'react';
 
 const TaskCreator = ({createTask}) => {
   const addTaskInput = useRef(null);
+
+  function taskLengthHandler(minTaskLength, maxTaskLength, callback, taskText) {
+    const taskLength = addTaskInput.current.value.length;
+    if (taskLength >= minTaskLength && taskLength <= maxTaskLength) {
+      callback(taskText);
+    } else {
+      alert(
+        `Task length must be minimum ${minTaskLength} and maximum ${maxTaskLength}`,
+      );
+    }
+  }
 
   return (
     <div className="task-creator-wrapper">
       <input ref={addTaskInput}></input>
       <button
         className="add-task-button"
-        onClick={() => createTask(addTaskInput.current.value)}
+        onClick={() =>
+          taskLengthHandler(1, 33, createTask, addTaskInput.current.value)
+        }
       >
         Add
       </button>
