876610a99bbbd9d909f94c22f335e0587c2738dc
Pavel Didenko <patriot2006_06@mail.ru>
2023-09-29 20:50:50 +0300

    feat: implement input focus after adding a task

---
+++ homeworks/pavel-didenko/20-react-api/src/App.jsx
@@ -0,0 +1,79 @@
+import './App.css';
+import Task from './components/Task/Task';
+import TaskCreator from './components/TaskCreator/TaskCreator';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if(storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    } 
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  function createTask(newTaskText) {
+    setTasks(tasks => [...tasks, {id: Date.now(), taskText: newTaskText, completed: false}]);
+  }
+
+
+  function modifyTasks (index, text) {
+    setTasks(tasks => {
+      return tasks.map((task, i) => {
+        if (i !== index) {
+          return task;
+        } else {
+          task.taskText = text
+          task.id = Date.now();
+          return task;
+        }
+      });
+    });
+  }
+
+  function setCompletedTask(index) {
+    setTasks(tasks => {
+      return tasks.map((task, i) => {
+        if(i !== index) {
+          return task;
+        } else {
+          task.completed = !task.completed;
+          return task;
+        }
+      })
+    })
+  }
+
+  function removeTask(index) {
+    setTasks(tasks.filter((_, i) => i !== index));
+  }
+  
+  return (
+    <div className="main-container">
+      <TaskCreator createTask={createTask} tasks={tasks} />
+      <div className="tasks-section">
+        {tasks.map((task, index) => {
+          return (
+            <Task
+              taskText={task.taskText}
+              key={task.id}
+              index={index}
+              completed={task.completed}
+              modifyTasks={modifyTasks}
+              removeTask={removeTask}
+              setCompletedTask={setCompletedTask}
+              setTasks={setTasks}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ homeworks/pavel-didenko/20-react-api/src/components/Task/Task.jsx
@@ -0,0 +1,84 @@
+import React, {useRef, useState} from 'react';
+import modifyIcon from '../../assets/img/icons/write.svg';
+import removeIcon from '../../assets/img/icons/delete.svg';
+import completeIcon from '../../assets/img/icons/task-complete.svg';
+import './task.css';
+
+const taskCompletedStyles = {
+  textDecoration: 'line-through',
+  color: 'green',
+};
+
+const Task = ({
+  taskText,
+  modifyTasks,
+  index,
+  removeTask,
+  completed,
+  setCompletedTask,
+}) => {
+  const [disabledModification, setDisabledModification] = useState(true);
+  const taskField = useRef(null);
+  const [opacity, setOpacity] = useState(0);
+
+  function modifyTaskHandler(newTaskText) {
+    if (disabledModification) {
+      setDisabledModification(false);
+    } else {
+      setDisabledModification(true);
+      modifyTasks(index, newTaskText);
+    }
+  }
+
+  function taskLengthValidator(task, minLength, maxLength) {
+    if(task.length >= minLength && task.length <= maxLength) {
+      modifyTaskHandler(task);
+    } else {
+      if(opacity === 0){
+        setOpacity(1);
+      }
+    }
+  }
+
+  return (
+    <div className="task" onClick={e => e.stopPropagation()}>
+      <div className="task__inputs-wrapper">
+        <input
+          className="task__input"
+          defaultValue={taskText}
+          disabled={disabledModification}
+          ref={taskField}
+          style={completed ? taskCompletedStyles : {}}
+          onKeyUp={e => {
+            if (e.key === 'Enter') {
+              taskLengthValidator(taskField.current.value, 1, 33);
+            }
+          }}
+        ></input>
+        <img
+          className="task__icon"
+          src={modifyIcon}
+          alt="Modify task"
+          onClick={() => taskLengthValidator(taskField.current.value, 1, 33)}
+        />
+        <img
+          className="task__icon"
+          src={removeIcon}
+          alt="Remove task"
+          onClick={() => removeTask(index)}
+        />
+        <img
+          className="task__icon"
+          src={completeIcon}
+          alt="Complete icon"
+          onClick={() => setCompletedTask(index)}
+        />
+      </div>
+      <p className="task-creator__warning" style={{opacity: opacity}}>
+        Task length must be from 1 to 33 characters
+      </p>
+    </div>
+  );
+};
+
+export default Task;
