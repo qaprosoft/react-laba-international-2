b6943ec099f89625bc3bca43c0313be753abaae9
Pavel Didenko <patriot2006_06@mail.ru>
2023-09-30 21:49:36 +0300

    refactor: remove extra code

--- homeworks/pavel-didenko/20-react-api/src/components/TaskCreator/TaskCreator.jsx
+++ homeworks/pavel-didenko/20-react-api/src/components/TaskCreator/TaskCreator.jsx
@@ -1,43 +1,19 @@
-import React, {useEffect, useMemo} from 'react';
 import './taskCreator.css';
 import {useRef, useState, useContext} from 'react';
 import {MainContext} from '../../contexts/mainContext';
 import useLengthHandler from '../../hooks/useLengthHandler';
 
 const taskExistsMessage = 'The task already exists';
-const taskLengthMessage = 'Task length must be from 0 to 33 characters';
+const taskLengthMessage = 'Task length must be from 1 to 33 characters';
 
 const TaskCreator = ({state}) => {
   const addTaskInput = useRef(null);
-  // const [opacity, setOpacity] = useState(0);
-  // const [taskExists, setTaskExists] = useState(false);
   const [taskText, setTastText] = useState('');
   const {createTask} = useContext(MainContext);
 
 
+
   const {taskExists, opacity, result} = useLengthHandler(1, 33, taskText, state);
-
-  // function taskLengthHandler(minLength, maxLength, createTask, taskText) {
-  //   const taskLength = taskText.length;
-  //   if (!validateDublicatedTask(taskText)) {
-  //     setTaskExists(true);
-  //     setOpacity(1);
-  //     addTaskInput.current.focus();
-  //     return;
-  //   }
-  //   if (taskLength >= minLength && taskLength <= maxLength) {
-  //     createTask(taskText);
-  //     addTaskInput.current.focus();
-  //     if (opacity === 1) {
-  //       setOpacity(0);
-  //     }
-
-  //   } else {
-  //     setTaskExists(false);
-  //     setOpacity(1);
-  //     addTaskInput.current.focus();
-  //   }
-  // }
 
   return (
     <div className="task-creator-wrapper">
@@ -47,6 +23,12 @@ const TaskCreator = ({state}) => {
           placeholder="Create-Todo-Task"
           onChange={e => {
             setTastText(e.target.value);
+          }}
+          onKeyUp={(e) => {
+            if(e.key === "Enter" && result){
+              createTask(taskText);
+              addTaskInput.current.focus();
+            }
           }}
         ></input>
         <p className="task-creator__warning" style={{opacity: opacity}}>
