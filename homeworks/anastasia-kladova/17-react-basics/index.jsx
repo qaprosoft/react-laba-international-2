9ceff2d669ad5e35899c0fd67b515c2d2db7c8a0
Nastya Kladova <nstsologub@gmail.com>
2023-10-01 15:44:01 +0200

    feat: implement traffic light functionality

--- homeworks/anastasia-kladova/17-react-basics/index.jsx
+++ homeworks/anastasia-kladova/17-react-basics/index.jsx
@@ -1,6 +1,27 @@
 const {useState, useEffect} = React;
 
 const App = () => {
+  const trafficStates = {
+    red: {
+      backgroundColor: 'red',
+      next: 'yellow',
+    },
+    yellow: {backgroundColor: 'yellow', next: 'green'},
+    green: {backgroundColor: 'green', next: 'red'},
+  };
+
+  const [currentColor, setCurrentColor] = useState('green');
+
+  useEffect(() => {
+    const {next} = trafficStates[currentColor];
+    const timerId = setTimeout(() => {
+      setCurrentColor(next);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [currentColor]);
   return (
     <div className="traffic-light">
       <div className="traffic-light__head"></div>
