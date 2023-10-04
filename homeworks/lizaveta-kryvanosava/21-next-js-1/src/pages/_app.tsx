70d04a3e0a45e0162a3a752cf3ff55f79a2e29a9
Rivenrem <rivensenik@gmail.com>
2023-10-04 23:23:11 +0300

    feat: add NextNProgress

--- homeworks/lizaveta-kryvanosava/21-next-js-1/src/pages/_app.tsx
+++ homeworks/lizaveta-kryvanosava/21-next-js-1/src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <NextNProgress height={10} />
       <ToastContainer />
     </>
   );
