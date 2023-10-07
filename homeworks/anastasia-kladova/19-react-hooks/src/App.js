78ff09d78e85959ebdd3762a5fe42cb560bdafc6
Nastya Kladova <nstsologub@gmail.com>
2023-10-07 22:32:41 +0200

    init: start HW19

---
+++ homeworks/anastasia-kladova/19-react-hooks/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <section className="todo">
+        <div className="todo__container"></div>
+      </section>
+    </main>
+  );
+};
+
+export default App;
