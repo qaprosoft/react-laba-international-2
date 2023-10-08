c825261cdb98ae905524343f935bb4316cc743b6
Nastya Kladova <nstsologub@gmail.com>
2023-10-08 16:57:09 +0200

    feat: implement delete todo functionality

--- homeworks/anastasia-kladova/19-react-hooks/src/components/TodoItem/TodoItem.jsx
+++ homeworks/anastasia-kladova/19-react-hooks/src/components/TodoItem/TodoItem.jsx
@@ -2,8 +2,17 @@ import {useContext} from 'react';
 import IconButton from '../Buttons/IconButton/IconButton';
 import styles from './TodoItem.module.css';
 import {Context} from '../../contexts/AppContext/AppContext';
+import {saveDataToStorage} from '../../utils/saveDataToStorage';
 
 const TodoItem = ({id, text}) => {
+  const {todos, setTodos} = useContext(Context);
+
+  const deleteTodo = id => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+    saveDataToStorage(newTodos);
+  };
+
   return (
     <li className={`todo__todo-item ${styles.todoItem}`} id={id}>
       <div className={styles.todoItem__box}>
@@ -12,7 +21,11 @@ const TodoItem = ({id, text}) => {
         </div>
         <div className={styles.todoItem__btns}>
           <IconButton type="button" classType="iconBtn--update" />
-          <IconButton type="button" classType="iconBtn--delete" />
+          <IconButton
+            type="button"
+            classType="iconBtn--delete"
+            onBtnClickHandler={() => deleteTodo(id)}
+          />
         </div>
       </div>
     </li>
