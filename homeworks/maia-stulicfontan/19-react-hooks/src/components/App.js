203b16c46c2bcc48f8ac32f7c4ee3773d0b9eae6
maiastulicfontan <maiastulicfontan@gmail.com>
2023-10-09 17:23:24 -0300

    fix: replace useReducer, remove unnecessary code

--- homeworks/maia-stulicfontan/19-react-hooks/src/components/App.js
+++ homeworks/maia-stulicfontan/19-react-hooks/src/components/App.js
@@ -1,13 +1,12 @@
 import NewTask from './NewTask';
 import Tasks from './Tasks';
 import validateTask from '../utils/validateTask';
-import tasksReducer from '../utils/tasksReducer';
 import {useEffect} from 'react';
-import {useReducer} from 'react';
+import {useState} from 'react';
 
 function App() {
   const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  const [tasks, dispatch] = useReducer(tasksReducer, savedTasks);
+  const [tasks, setTasks] = useState(savedTasks);
 
   useEffect(() => {
     if (tasks) {
@@ -17,12 +16,14 @@ function App() {
 
   const handleAddTask = taskText => {
     if (validateTask(tasks, taskText)) {
-      dispatch({
-        type: 'added',
-        text: taskText,
-        id: crypto.randomUUID(),
-        isCompleted: false,
-      });
+      setTasks([
+        ...tasks,
+        {
+          text: taskText,
+          id: crypto.randomUUID(),
+          isCompleted: false,
+        },
+      ]);
     }
   };
 
@@ -33,16 +34,24 @@ function App() {
         newText,
       )
     ) {
-      dispatch({type: 'edited', id: taskId, text: newText});
+      setTasks(
+        tasks.map(task =>
+          task.id === taskId ? {...task, text: newText} : {...task},
+        ),
+      );
     }
   };
 
   const handleDeleteTask = taskId => {
-    dispatch({type: 'deleted', id: taskId});
+    setTasks(tasks.filter(task => task.id !== taskId));
   };
 
   const handleCompleteTask = (taskId, isCompleted) => {
-    dispatch({type: 'completed', id: taskId, isCompleted: isCompleted});
+    setTasks(
+      tasks.map(task =>
+        task.id === taskId ? {...task, isCompleted: isCompleted} : {...task},
+      ),
+    );
   };
 
   return (
@@ -50,13 +59,13 @@ function App() {
       <header className="header">
         <h1 className="title">To-Do List</h1>
       </header>
-      <NewTask onAdd={handleAddTask}></NewTask>
+      <NewTask onAdd={handleAddTask} />
       <Tasks
         tasks={tasks}
         onEdit={handleEditTask}
         onDelete={handleDeleteTask}
         onComplete={handleCompleteTask}
-      ></Tasks>
+      />
     </div>
   );
 }
--- homeworks/maia-stulicfontan/19-react-hooks/src/components/Task.js
+++ homeworks/maia-stulicfontan/19-react-hooks/src/components/Task.js
@@ -7,69 +7,67 @@ function Task({task, isCompleted, onEdit, onDelete, onComplete}) {
   const [taskCompleted, setTaskCompleted] = useState(isCompleted);
   const [inputValue, setInputValue] = useState(task.text);
   return (
-    <>
-      <div className="task">
-        <div className="task__container">
-          <input
-            type="checkbox"
-            className="task__checkbox"
-            checked={taskCompleted}
-            disabled={inputEnabled}
-            onChange={() => {
-              setTaskCompleted(!taskCompleted);
-              onComplete(task.id, !taskCompleted);
+    <div className="task">
+      <div className="task__container">
+        <input
+          type="checkbox"
+          className="task__checkbox"
+          checked={taskCompleted}
+          disabled={inputEnabled}
+          onChange={() => {
+            setTaskCompleted(!taskCompleted);
+            onComplete(task.id, !taskCompleted);
+          }}
+        ></input>
+        <input
+          type="text"
+          className={`task__text ${isCompleted ? 'task--completed' : ''}`}
+          readOnly={!inputEnabled}
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        ></input>
+        {!isCompleted && (
+          <button
+            className="task__button"
+            onClick={() => {
+              setInputEnabled(true);
             }}
-          ></input>
-          <input
-            type="text"
-            className={`task__text ${isCompleted ? 'task--completed' : ''}`}
-            readOnly={!inputEnabled}
-            value={inputValue}
-            onChange={e => setInputValue(e.target.value)}
-          ></input>
-          {!isCompleted && (
-            <button
-              className="task__button"
-              onClick={() => {
-                setInputEnabled(true);
-              }}
-            >
-              <img className="task__img" src={editIcon} alt="Edit"></img>
-            </button>
-          )}
-          <button className="task__button" onClick={() => onDelete(task.id)}>
-            <img className="task__img" src={deleteIcon} alt="Delete"></img>
+          >
+            <img className="task__img" src={editIcon} alt="Edit"></img>
+          </button>
+        )}
+        <button className="task__button" onClick={() => onDelete(task.id)}>
+          <img className="task__img" src={deleteIcon} alt="Delete"></img>
+        </button>
+      </div>
+      {inputEnabled && (
+        <div className="edit-buttons__container">
+          <button
+            className="edit-buttons__button button--green"
+            onClick={() => {
+              try {
+                onEdit(task.id, inputValue);
+                setInputEnabled(false);
+              } catch (error) {
+                alert(error);
+              }
+            }}
+            disabled={inputValue.length === 0 || task.text === inputValue}
+          >
+            Save
+          </button>
+          <button
+            className="edit-buttons__button button--red"
+            onClick={() => {
+              setInputEnabled(false);
+              setInputValue(task.text);
+            }}
+          >
+            Cancel
           </button>
         </div>
-        {inputEnabled && (
-          <div className="edit-buttons__container">
-            <button
-              className="edit-buttons__button button--green"
-              onClick={() => {
-                try {
-                  onEdit(task.id, inputValue);
-                  setInputEnabled(false);
-                } catch (error) {
-                  alert(error);
-                }
-              }}
-              disabled={inputValue.length === 0 || task.text === inputValue}
-            >
-              Save
-            </button>
-            <button
-              className="edit-buttons__button button--red"
-              onClick={() => {
-                setInputEnabled(false);
-                setInputValue(task.text);
-              }}
-            >
-              Cancel
-            </button>
-          </div>
-        )}
-      </div>
-    </>
+      )}
+    </div>
   );
 }
 
