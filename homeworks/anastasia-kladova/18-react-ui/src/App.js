670452f3eea16bcf70907058a7f2e31eaf5c64a3
Nastya Kladova <nstsologub@gmail.com>
2023-10-09 22:54:05 +0200

    feat: add responsive styles

--- homeworks/anastasia-kladova/18-react-ui/src/App.js
+++ homeworks/anastasia-kladova/18-react-ui/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const refreshAll = async () => {
     if (!avatarsLength) return;
     const data = await getNewAvatar(avatarsLength);
-    setAvatars([...data]);
+    setAvatars([data]);
   };
 
   return (
