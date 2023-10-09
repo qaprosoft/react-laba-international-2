dfe99172d1f40124becfc1c8edf55ba7ce5c0304
Babenko Alexander <39292919+babenko-alexander@users.noreply.github.com>
2023-10-09 19:46:05 +0300

    Todo item updates

--- homeworks/svitlana-kovalenko/19-react-hooks/src/Todo.jsx
+++ homeworks/svitlana-kovalenko/19-react-hooks/src/Todo.jsx
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import write from './img/write.png';
 import trash from './img/delete.png';
 
-export const Todo = ({ task, handleCompleted, handleDelete, handleEdit, edition }) => {
-  const [value, setValue] = useState('');
-  const handleSubmit = e => {
-    e.preventDefault();
+export const Todo = ({
+  task,
+  handleCompleted,
+  handleDelete,
+  handleEdit,
+  edition,
+}) => {
+  const [value, setValue] = useState(task.task);
 
+  const handleSubmit = e => {
+    // e.preventDefault();
+
+    // !!!UPDATE TASK HERE INSTEAD OF HANDLING EDIT STATE
     handleEdit(task.id);
   };
+
+  console.log('task:::', task);
+
   return (
     <>
       <div className="todos_wrap">
-        <div className="todos_item" onClick={() => handleEdit(task.id)}>
-          <input className={`${task.complete ? 'completed' : ''}`} type="text" value={value} placeholder={task.task} onChange={e => setValue(e.target.value)} onFocus={() => setValue(task.task)} />
-        </div>
-        {task.edition ? <button type="submit" onClick={handleSubmit}>Update</button> : <div>
-          <img
-            src={write}
-            onClick={() => handleCompleted(task.id)}
-            alt="complete"
+        <div
+          className="todos_item"
+          // onClick={() => handleEdit(task.id)}
+        >
+          <input
+            className={`${task.complete ? 'completed' : ''}`}
+            type="text"
+            value={value}
+            // placeholder={task.task}
+            placeholder={'Enter task'}
+            // onChange={e => setValue(e.target.value)}
+            onChange={e => setValue(e.target.value)}
+            onFocus={() => handleEdit(task.id)}
           />
-          <img src={trash} alt="delete" onClick={() => handleDelete(task.id)} />
-        </div>}
-
+        </div>
+        {task.edition ? (
+          <button type="submit" onClick={handleSubmit}>
+            Update
+          </button>
+        ) : (
+          <div>
+            <img
+              src={write}
+              onClick={() => handleCompleted(task.id)}
+              alt="complete"
+            />
+            <img
+              src={trash}
+              alt="delete"
+              onClick={() => handleDelete(task.id)}
+            />
+          </div>
+        )}
       </div>
     </>
   );
