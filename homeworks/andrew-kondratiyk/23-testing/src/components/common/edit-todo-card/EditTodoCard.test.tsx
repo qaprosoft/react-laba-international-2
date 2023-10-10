05a597858a40a2db184dc86803ffb93db93fb5e4
Andrew <gandrejko2002@gmail.com>
2023-10-10 21:21:58 +0300

    + setup tests configuration and added tests for edit todo card

---
+++ homeworks/andrew-kondratiyk/23-testing/src/components/common/edit-todo-card/EditTodoCard.test.tsx
@@ -0,0 +1,35 @@
+import EditTodoCard from '@/components/common/edit-todo-card/EditTodoCard';
+import {renderWithClient} from '@/tests/utils';
+import {screen, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+const mockTodo = {
+  _id: '1',
+  userId: '1',
+  title: 'Test Todo',
+  completed: false,
+};
+
+describe('EditTodoCard', () => {
+  it('renders without crashing', async () => {
+    renderWithClient(<EditTodoCard />);
+    expect(
+      screen.getByPlaceholderText('What are you working on?'),
+    ).toBeInTheDocument();
+  });
+
+  it('renders with todo data', async () => {
+    renderWithClient(<EditTodoCard todo={mockTodo} />);
+    expect(screen.getByDisplayValue(mockTodo.title)).toBeInTheDocument();
+  });
+
+  it('calls onCancel callback when cancel button is clicked', () => {
+    const onCancelMock = jest.fn();
+    renderWithClient(<EditTodoCard onCancel={onCancelMock} />);
+
+    const cancelButton = screen.getByText('Cancel');
+    fireEvent.click(cancelButton);
+
+    expect(onCancelMock).toHaveBeenCalled();
+  });
+});
