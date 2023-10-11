af53873ce18b43b86094289d86d03d71b2f97ab0
BrunoDiaz7 <brunoleandrodiaz07@gmail.com>
2023-10-10 21:53:57 -0300

    Lecture #19 homework

---
+++ homeworks/bruno-diaz/19-React-hooks/react-hooks/src/components/Form.js
@@ -0,0 +1,17 @@
+export function Form(props) {
+
+    const {task, handleSubmit, handleChange} = props
+  return (
+    <form onSubmit ={handleSubmit}>
+      <input
+        type="text"
+        className="input__form"
+        onChange={handleChange}
+        value={task}
+      />
+      <button className="input__button" onChange={handleSubmit}>
+        Add
+      </button>
+    </form>
+  );
+}
