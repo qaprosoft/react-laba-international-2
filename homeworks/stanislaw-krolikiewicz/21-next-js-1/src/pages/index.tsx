561e0f7616a0548b4859b2da6ac649c99fa1773e
stkrolikiewicz <st.krolikiewicz@gmail.com>
2023-10-11 14:26:57 +0200

    feat: create separate pages for SSG & SSG & navigation

--- homeworks/stanislaw-krolikiewicz/21-next-js-1/src/pages/index.tsx
+++ homeworks/stanislaw-krolikiewicz/21-next-js-1/src/pages/index.tsx
@@ -1,5 +1,20 @@
-import {Main} from '@/views';
+import Link from 'next/link';
 
 export default function Home() {
-  return <Main />;
+  return (
+    <>
+      <main>
+        <div>
+          <div style={{marginBottom: '20px'}}>
+            <Link href="/ssg">SSG</Link>
+          </div>
+          <div style={{marginBottom: '20px'}}>
+            <Link href="/ssr">SSR</Link>
+          </div>
+        </div>
+
+        <h1>Welcome To The Home Page</h1>
+      </main>
+    </>
+  );
 }
