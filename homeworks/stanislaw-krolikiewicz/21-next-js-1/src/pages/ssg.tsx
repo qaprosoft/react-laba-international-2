3737ac956990fd4e99df2940248e5fd2d8acdb12
stkrolikiewicz <st.krolikiewicz@gmail.com>
2023-10-11 15:20:51 +0200

    feat: implement Static Site Generation and update Main view component

--- homeworks/stanislaw-krolikiewicz/21-next-js-1/src/pages/ssg.tsx
+++ homeworks/stanislaw-krolikiewicz/21-next-js-1/src/pages/ssg.tsx
@@ -1,5 +1,21 @@
 import {Main} from '@/views';
+import {getAvatars} from '@/services/avatars';
+import {Avatar} from '@/types';
 
-export default function Home() {
-  return <Main />;
+export async function getStaticProps() {
+  const avatars = await getAvatars(5);
+
+  return {
+    props: {
+      avatars,
+    },
+  };
+}
+
+interface SSGProps {
+  avatars: Avatar[];
+}
+
+export default function SSG({avatars}: SSGProps) {
+  return <Main avatars={avatars} />;
 }
--- homeworks/stanislaw-krolikiewicz/21-next-js-1/src/views/Main/Component.tsx
+++ homeworks/stanislaw-krolikiewicz/21-next-js-1/src/views/Main/Component.tsx
@@ -3,19 +3,25 @@ import {TilesGrid} from './_components';
 import {ReactElement, useEffect, useState} from 'react';
 import {Avatar} from '@/types';
 import {AnimatePresence} from 'framer-motion';
-import {API_URL_SINGLE_AVATAR, API_URL_MULTIPLE_AVATARS} from '@/constants';
+import {getAvatars, getSingleAvatar} from '@/services/avatars';
 
-export default function Main() {
+interface MainProps {
+  avatars: Avatar[];
+}
+
+export default function Main({avatars}: MainProps) {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [tiles, setTiles] = useState<ReactElement[]>([]);
+  const [tiles, setTiles] = useState<ReactElement[]>(
+    avatars.map((avatar: Avatar) => (
+      <Tile key={avatar.id} avatarUrl={avatar.url} />
+    )),
+  );
 
   const addNewTile = async () => {
     setLoading(true);
     try {
-      const res = await fetch(API_URL_SINGLE_AVATAR);
-      const data = await res.json();
-      if (!data.length) throw new Error('Failed to fetch avatar');
+      const data = await getSingleAvatar();
       setTiles([...tiles, <Tile key={data[0].id} avatarUrl={data[0].url} />]);
       setError(null);
     } catch (error) {
@@ -28,9 +34,7 @@ export default function Main() {
   const fetchAvatars = async () => {
     setLoading(true);
     try {
-      const res = await fetch(API_URL_MULTIPLE_AVATARS(tiles.length));
-      const data = await res.json();
-      if (!data.length) throw new Error('Failed to fetch avatars!');
+      const data = await getAvatars(tiles.length);
       setTiles(
         data.map((avatar: Avatar) => (
           <Tile key={avatar.id} avatarUrl={avatar.url} />
@@ -45,16 +49,13 @@ export default function Main() {
   };
 
   useEffect(() => {
-    if (tiles.length) fetchAvatars();
-  }, []);
-
-  useEffect(() => {
     if (error) {
       setTimeout(() => {
         setError(null);
       }, 3000);
     }
   }, [error]);
+
   return (
     <main className="p-[40px] w-full flex flex-col items-center">
       <TilesGrid
