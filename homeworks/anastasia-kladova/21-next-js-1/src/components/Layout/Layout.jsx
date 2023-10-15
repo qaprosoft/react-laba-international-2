78b75952b5f8c3abdb6eb7907e2758525b3865d7
Nastya Kladova <nstsologub@gmail.com>
2023-10-15 17:58:30 +0200

    add hw 21

---
+++ homeworks/anastasia-kladova/21-next-js-1/src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+
+
+const Layout = ({children}) => {
+    return (
+        <>
+        <Header/>
+          {children} 
+       </>
+    )
+};
+
+export default Layout;
