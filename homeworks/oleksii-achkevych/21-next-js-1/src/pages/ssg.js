3e6d9da1f8b29036177ad2bcaede0595552e3d07
ste1nerr <oachkevych@gmail.com>
2023-10-15 12:44:02 +0300

    done all tasks

---
+++ homeworks/oleksii-achkevych/21-next-js-1/src/pages/ssg.js
@@ -0,0 +1,21 @@
+// pages/ssg.js
+
+import HomePage from '../components/HomePage/HomePage'; 
+
+function SSGPage({ avatars }) {
+  return <HomePage avatars={avatars} />;
+}
+
+export async function getStaticProps() {
+  const API_URL = 'https://tinyfac.es/api/data';
+  const response = await fetch(`${API_URL}?limit=5&quality=0`);
+  const avatars = await response.json();
+
+  return {
+    props: {
+      avatars,
+    },
+  };
+}
+
+export default SSGPage;
---
+++ homeworks/oleksii-achkevych/21-next-js-1/src/pages/ssr.js
@@ -0,0 +1,19 @@
+import HomePage from '../components/HomePage/HomePage'; 
+
+function SSRPage({ avatars }) {
+  return <HomePage avatars={avatars} />;
+}
+
+export async function getServerSideProps() {
+  const API_URL = 'https://tinyfac.es/api/data';
+  const response = await fetch(`${API_URL}?limit=5&quality=0`);
+  const avatars = await response.json();
+
+  return {
+    props: {
+      avatars,
+    },
+  };
+}
+
+export default SSRPage;
