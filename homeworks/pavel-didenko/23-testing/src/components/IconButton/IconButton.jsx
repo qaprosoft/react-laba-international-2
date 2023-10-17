c78aa4aaf33548293bdfe43cd34a5890dd2cade9
Pavel Didenko <patriot2006_06@mail.ru>
2023-10-18 01:06:48 +0300

    feat: add more tests for IconButton, Task, TaskCreator components

--- homeworks/pavel-didenko/23-testing/src/components/IconButton/IconButton.jsx
+++ homeworks/pavel-didenko/23-testing/src/components/IconButton/IconButton.jsx
@@ -5,7 +5,6 @@ const IconButton = ({src, clickHandler, alt}) => {
   return (
     <button className="task__button">
       <img
-        data-testid="task-icon"
         className="task__icon"
         src={src}
         alt={alt}
--- homeworks/pavel-didenko/23-testing/src/components/IconButton/IconButton.test.jsx
+++ homeworks/pavel-didenko/23-testing/src/components/IconButton/IconButton.test.jsx
@@ -3,15 +3,17 @@ import renderer from 'react-test-renderer';
 import buttonImage from '../../assets/img/icons/task-incomplete.svg';
 import {render, screen} from '@testing-library/react';
 
-
+const altText = 'Complete icon';
 
 it('renders correctly', () => {
-  const tree = renderer.create(<IconButton src={buttonImage} />).toJSON();
+  const tree = renderer
+    .create(<IconButton src={buttonImage} alt={'Complete icon'} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Has image', () => {
-  render(<IconButton src={buttonImage} />);
-  const img = screen.getByTestId('task-icon');
+  render(<IconButton src={buttonImage} alt={altText} />);
+  const img = screen.getByAltText(altText);
   expect(img.getAttribute("src")).toMatch(buttonImage);
 });
