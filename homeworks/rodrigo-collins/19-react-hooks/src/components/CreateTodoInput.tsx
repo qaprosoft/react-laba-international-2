ff4d78652d0dea05f4afddccee3856b4d27d990c
Rodrigo Collins <rodri_collins@hotmail.com>
2023-10-17 12:51:28 -0300

    fix: remove alerts and add error msj

--- homeworks/rodrigo-collins/19-react-hooks/src/components/CreateTodoInput.tsx
+++ homeworks/rodrigo-collins/19-react-hooks/src/components/CreateTodoInput.tsx
@@ -5,25 +5,35 @@ interface CreateTodoInputProps {
     setTask: (value: string) => void;
     inputRef: React.RefObject<HTMLInputElement>;
     addTodo: () => void;
+    error: string | null;
+    setError: (value: string) => void;
 }
 
-const CreateTodoInput: React.FC<CreateTodoInputProps> = ({ task, setTask, inputRef, addTodo }) => {
+const CreateTodoInput: React.FC<CreateTodoInputProps> = ({ task, setTask, inputRef, addTodo, error, setError }) => {
 
     const handleKeyDown = (event: React.KeyboardEvent) => {
+        setError('')
         if (event.key === 'Enter') {
             addTodo();
         }
     };
     return (
-        <input
-            ref={inputRef}
-            className='todo-input'
-            type="text"
-            placeholder="Create Todo-Task"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            onKeyDown={handleKeyDown}
-        />
+        <div className='todo-input-container'>
+            <input
+                ref={inputRef}
+                className='todo-input'
+                type="text"
+                placeholder="Create Todo-Task"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                onKeyDown={handleKeyDown}
+                onBlur={() => setError('')}
+            />
+            {
+                error && <div className='error-msj'>{error}</div>
+            }
+        </div>
+
     );
 };
 
