47447072e91779fa58123ec1ad9b475463d09596
ViVse <ivanisenko730@gmail.com>
2023-10-19 14:38:24 +0300

    feat: created ssg page

--- homeworks/ivan-ishchenko/21-next-js-1/src/pages/_document.js
+++ homeworks/ivan-ishchenko/21-next-js-1/src/pages/_document.js
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <div className="menu">
+          <Link href="/" className="mr-10 font-bold">
+            Home
+          </Link>
+          <Link href="ssr" className="mr-10 font-bold">
+            SSR
+          </Link>
+          <Link href="ssg" className="font-bold">
+            SSG
+          </Link>
+        </div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
